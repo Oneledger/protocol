9886ad0deb7d6669096108265b8f62e93029c433
Yang Li <stephen.liy@gmail.com>
2018-03-20 23:42:10 -0400

    add coinbase and transaction

--- core/transaction.go
+++ core/transaction.go
@@ -1,5 +1,7 @@
 package core
 
+import "fmt"
+
 type TxInput struct {
   TxId []byte
   Value int
@@ -13,6 +15,18 @@ type TxOutput struct {
 
 type Transaction struct {
   Id []byte
-  Input TxInput
-  Output TxOutput
+  Input []TxInput
+  Output []TxOutput
+}
+
+const reward = 1024
+
+func CoinbaseTx(to string, data string) *Transaction {
+  if data == "" {
+    data = "This is the beginning of Oneledger network"
+  }
+  txin := TxInput{[]byte{}, 0, data}
+  txout := TxOutput{reward, to}
+  tx := Transaction{nil, []TxInput{txin}, []TxOutput{txout}}
+  return &tx
 }
