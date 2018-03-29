c3dca24dff184e7f31c2a0d42642ce93e7ea8058
alexjlan <jielan@oneledger.io>
2018-03-28 22:21:11 -0400

    #156330676:  modify basic_state interface

--- consensus/states/basic_state.go
+++ consensus/states/basic_state.go
@@ -5,7 +5,7 @@ import (
 )
 
 type BasicState interface {
-	Enter (previous BasicState, event types.Event)
-	Exit (next BasicState, event types.Event )
+	Enter (event types.Event) *BasicState
+	Exit (event types.Event) *BasicState
 	Process ()
 }
---
+++ consensus/states/propose.go
@@ -0,0 +1,8 @@
+package states
+
+import "github.com/Oneledger/prototype/consensus/types"
+
+type Propose struct {
+	Block *types.Block
+}
+
