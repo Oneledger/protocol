ffefa895e8afdde0814b1a50cc37cbd86381474f
Yang Li <stephen.liy@gmail.com>
2018-03-28 22:21:35 -0400

    add output transaction TODO: add data reference to the data structure

---
+++ core/tx_out.go
@@ -0,0 +1,27 @@
+package core
+
+import (
+  "../utils",
+  "bytes"
+)
+
+type TxOutput struct {
+  Value int
+  PubKeyHash []byte
+}
+
+func (out *TxOutput) AssignPubKeyHash(address []byte) {
+  pubKeyHash := utils.Base58Decode(address)
+  pubKeyHash = pubKeyHash[1:len(pubKeyHash) - 4]
+  out.PubKeyHash = pubKeyHash
+}
+
+func (out *TxOutput) VerifyTxOutput(pubKeyHash []byte) bool {
+  return bytes.Compare(out.PubKeyHash, pubKeyHash) == 0
+}
+
+func NewTxOutput(value int, address string) *TxOutput {
+  output := TxOutput{value, nil}
+  output.AssignPubKeyHash([]byte(address))
+  return &output
+}
