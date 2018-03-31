7019ee1e8d05c07c33affc83edd2d74032896805
Yang Li <stephen.liy@gmail.com>
2018-03-31 00:24:00 -0400

    add more method for transaction.NOT FINISHED YET

--- core/tx.go
+++ core/tx.go
@@ -3,6 +3,14 @@ package core
 import (
   "../utils"
   "crypto/sha256"
+  "crypto/ecdsa"
+  "encoding/hex"
+  "crypto/rand"
+
+  "github.com/jinzhu/copier"
+  "log"
+  "fmt"
+
 )
 
 const subsidy = 10 //TODO: this will be removed
@@ -29,5 +37,53 @@ func DeserializeTx(data []byte) Tx {
 }
 
 func (tx *Tx) Hash () []byte {
-  return sha256.Sum256(tx.Serialize())[:]
+  txCloned := tx.clone()
+  txCloned.ID = []byte{}
+  return sha256.Sum256(txCloned.Serialize())[:]
 }
+
+func (tx *Tx) Clone() Tx {
+  txCloned := Tx{}
+  copier.Copy(&tx, &txCloned)
+  return txCloned
+}
+
+func (tx *Tx) Sign(privKey ecdsa.PrivateKey, prevTxs map[string]Tx) Tx{
+  if tx.IsCoinbase() {
+    return
+  }
+  for _, input := range tx.Inputs {
+    if prevTxs[hex.EncodeToString(input.Id)].Id == nil {
+      log.Panic("ERROR: Previous transaction is not correct")
+    }
+  }
+  txCloned := tx.Clone()
+
+  for inputId, input := range txCloned.Inputs {
+    prevTx := prevTxs[hex.EncodeToString(input.Id)]
+    txCloned.inputs[inputId].Signature = nil
+    txCloned.inputs[inputId].PubKey = prevTx.outpus[input.OutputIndex].PubKeyHash
+    dataToSign := fmt.Sprintf("%x\n", txCloned)
+    r, s, err := ecdsa.Sign(rand.Reader, &privKey, []byte(dataToSign))
+    if err != nil {
+      log.Panic(err)
+    }
+    signature := append(r.Bytes(), s.Bytes()...)
+    tx.Inputs[inputId].Signature = signature
+    txCloned.Inputs[inputId].PubKey = nil
+  }
+  return txCloned
+}
+
+
+
+
+
+
+
+
+
+
+
+
+func placeHolder() {}
