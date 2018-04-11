0d3abc32afeb68ce0ae577eaac0e22cb1df10ef5
alexjlan <jielan@oneledger.io>
2018-04-10 21:27:18 -0400

    [#156456190] modify block

--- consensus/types/block.go
+++ consensus/types/block.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
-	"io"
 	"strings"
 	"time"
 
@@ -15,11 +14,11 @@ import (
 	tps "github.com/tendermint/tendermint/types"
 )
 
-// Block defines the atomic unit for oneledger blockchain.
+// Block defines the atomic unit of a oneledger blockchain.
+// TODO: add Version byte
 type Block struct {
 	*Header    `json:"header"`
 	*Data      `json:"data"`
-	*Reference `json:"reference"`
 	Evidence   EvidenceData `json:"evidence"`
 	LastCommit *Commit      `json:"last_commit"`
 }
@@ -146,7 +145,7 @@ func (b *Block) StringShort() string {
 
 //-----------------------------------------------------------------------------
 
-// Header defines the structure of a Tendermint block header
+// Header defines the structure of a oneledger block header
 // TODO: limit header size
 // NOTE: changes to the Header should be duplicated in the abci Header
 type Header struct {
@@ -157,7 +156,7 @@ type Header struct {
 	NumTxs  int64     `json:"num_txs"`
 
 	// prev block info
-	LastBlockID BlockID `json:"last_block_id"`
+	LastBlockID tps.BlockID `json:"last_block_id"`
 	TotalTxs    int64   `json:"total_txs"`
 
 	// hashes of block data
@@ -235,154 +234,6 @@ func (h *Header) StringIndented(indent string) string {
 
 //-------------------------------------
 
-// Commit contains the evidence that a block was committed by a set of validators.
-// NOTE: Commit is empty for height 1, but never nil.
-type Commit struct {
-	// NOTE: The Precommits are in order of address to preserve the bonded ValidatorSet order.
-	// Any peer with a block can gossip precommits by index with a peer without recalculating the
-	// active ValidatorSet.
-	BlockID    BlockID `json:"blockID"`
-	Precommits []*Vote `json:"precommits"`
-
-	// Volatile
-	firstPrecommit *Vote
-	hash           cmn.HexBytes
-	bitArray       *cmn.BitArray
-}
-
-// FirstPrecommit returns the first non-nil precommit in the commit
-func (commit *Commit) FirstPrecommit() *Vote {
-	if len(commit.Precommits) == 0 {
-		return nil
-	}
-	if commit.firstPrecommit != nil {
-		return commit.firstPrecommit
-	}
-	for _, precommit := range commit.Precommits {
-		if precommit != nil {
-			commit.firstPrecommit = precommit
-			return precommit
-		}
-	}
-	return nil
-}
-
-// Height returns the height of the commit
-func (commit *Commit) Height() int64 {
-	if len(commit.Precommits) == 0 {
-		return 0
-	}
-	return commit.FirstPrecommit().Vote.Height
-}
-
-// Round returns the round of the commit
-func (commit *Commit) Round() int {
-	if len(commit.Precommits) == 0 {
-		return 0
-	}
-	return commit.FirstPrecommit().Vote.Round
-}
-
-// Type returns the vote type of the commit, which is always VoteTypePrecommit
-func (commit *Commit) Type() byte {
-	return VoteTypePrecommit
-}
-
-// Size returns the number of votes in the commit
-func (commit *Commit) Size() int {
-	if commit == nil {
-		return 0
-	}
-	return len(commit.Precommits)
-}
-
-// BitArray returns a BitArray of which validators voted in this commit
-func (commit *Commit) BitArray() *cmn.BitArray {
-	if commit.bitArray == nil {
-		commit.bitArray = cmn.NewBitArray(len(commit.Precommits))
-		for i, precommit := range commit.Precommits {
-			// TODO: need to check the BlockID otherwise we could be counting conflicts,
-			// not just the one with +2/3 !
-			commit.bitArray.SetIndex(i, precommit != nil)
-		}
-	}
-	return commit.bitArray
-}
-
-// GetByIndex returns the vote corresponding to a given validator index
-func (commit *Commit) GetByIndex(index int) *Vote {
-	return commit.Precommits[index]
-}
-
-// IsCommit returns true if there is at least one vote
-func (commit *Commit) IsCommit() bool {
-	return len(commit.Precommits) != 0
-}
-
-// ValidateBasic performs basic validation that doesn't involve state data.
-func (commit *Commit) ValidateBasic() error {
-	if commit.BlockID.IsZero() {
-		return errors.New("Commit cannot be for nil block")
-	}
-	if len(commit.Precommits) == 0 {
-		return errors.New("No precommits in commit")
-	}
-	height, round := commit.Height(), commit.Round()
-
-	// validate the precommits
-	for _, precommit := range commit.Precommits {
-		// It's OK for precommits to be missing.
-		if precommit == nil {
-			continue
-		}
-		// Ensure that all votes are precommits
-		if precommit.Vote.Type != VoteTypePrecommit {
-			return fmt.Errorf("Invalid commit vote. Expected precommit, got %v",
-				precommit.Vote.Type)
-		}
-		// Ensure that all heights are the same
-		if precommit.Vote.Height != height {
-			return fmt.Errorf("Invalid commit precommit height. Expected %v, got %v",
-				height, precommit.Vote.Height)
-		}
-		// Ensure that all rounds are the same
-		if precommit.Vote.Round != round {
-			return fmt.Errorf("Invalid commit precommit round. Expected %v, got %v",
-				round, precommit.Vote.Round)
-		}
-	}
-	return nil
-}
-
-// Hash returns the hash of the commit
-func (commit *Commit) Hash() cmn.HexBytes {
-	if commit.hash == nil {
-		bs := make([]merkle.Hasher, len(commit.Precommits))
-		for i, precommit := range commit.Precommits {
-			bs[i] = wireHasher(precommit)
-		}
-		commit.hash = merkle.SimpleHashFromHashers(bs)
-	}
-	return commit.hash
-}
-
-// StringIndented returns a string representation of the commit
-func (commit *Commit) StringIndented(indent string) string {
-	if commit == nil {
-		return "nil-Commit"
-	}
-	precommitStrings := make([]string, len(commit.Precommits))
-	for i, precommit := range commit.Precommits {
-		precommitStrings[i] = precommit.String()
-	}
-	return fmt.Sprintf(`Commit{
-%s  BlockID:    %v
-%s  Precommits: %v
-%s}#%v`,
-		indent, commit.BlockID,
-		indent, strings.Join(precommitStrings, "\n"+indent+"  "),
-		indent, commit.hash)
-}
 
 //-----------------------------------------------------------------------------
 
@@ -471,45 +322,6 @@ func (data *EvidenceData) StringIndented(indent string) string {
 		indent, strings.Join(evStrings, "\n"+indent+"  "),
 		indent, data.hash)
 	return ""
-}
-
-//--------------------------------------------------------------------------------
-
-// BlockID defines the unique ID of a block as its Hash and its PartSetHeader
-type BlockID struct {
-	Hash        cmn.HexBytes  `json:"hash"`
-	PartsHeader tps.PartSetHeader `json:"parts"`
-}
-
-// IsZero returns true if this is the BlockID for a nil-block
-func (blockID BlockID) IsZero() bool {
-	return len(blockID.Hash) == 0 && blockID.PartsHeader.IsZero()
-}
-
-// Equals returns true if the BlockID matches the given BlockID
-func (blockID BlockID) Equals(other BlockID) bool {
-	return bytes.Equal(blockID.Hash, other.Hash) &&
-		blockID.PartsHeader.Equals(other.PartsHeader)
-}
-
-// Key returns a machine-readable string representation of the BlockID
-func (blockID BlockID) Key() string {
-	return string(blockID.Hash) + string(wire.BinaryBytes(blockID.PartsHeader))
-}
-
-// WriteSignBytes writes the canonical bytes of the BlockID to the given writer for digital signing
-func (blockID BlockID) WriteSignBytes(w io.Writer, n *int, err *error) {
-	if blockID.IsZero() {
-		wire.WriteTo([]byte("null"), w, n, err)
-	} else {
-		wire.WriteJSON(tps.CanonicalBlockID(tps.BlockID{blockID.Hash,blockID.PartsHeader}), w, n, err)
-	}
-
-}
-
-// String returns a human readable string representation of the BlockID
-func (blockID BlockID) String() string {
-	return fmt.Sprintf(`%v:%v`, blockID.Hash, blockID.PartsHeader)
 }
 
 //-------------------------------------------------------
