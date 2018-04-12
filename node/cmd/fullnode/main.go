1ab35093b50364103ce33a1f3fba9809886d4694
Paul W. Homer <paul@oneledger.io>
2018-04-12 16:17:11 -0400

    Cleanup for the command line args.

--- node/cmd/fullnode/main.go
+++ node/cmd/fullnode/main.go
@@ -1,5 +1,7 @@
 /*
 	Copyright 2017-2018 OneLedger
+
+	A fullnode for the OneLedger chain. Includes cli arguments to initialize, restart, etc.
 */
 
 package main
@@ -17,34 +19,36 @@ import (
 
 // Setup a basic command for testing
 var start = &cobra.Command{
-	Run:   Start,
-	Use:   "start",
-	Short: "Start",
-	Long:  "Start",
+	Run:   StartNode,
+	Use:   "node",
+	Short: "StartNode",
+	Long:  "Start a full node",
 }
 
 var service common.Service
+var logger log.Logger
 
 func main() {
-	//logger := log.TestingLogger()
-	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
-
-	logger.Info("Starting")
-	//fmt.Println("Staring OneLedger Node")
-
-	// TODO: Initialize Cobra (cli) and Viper (config)
-
-	node := app.NewApplicationContext()
-	_ = node
-
-	// TODO: Just use a generic base, not hooked up yet.
-	//app := types.NewBaseApplication()
-	app := *node
-
-	service = server.NewGRPCServer("unix://data.sock", types.NewGRPCApplication(app))
-	service.SetLogger(logger)
+	Initialize()
+	ParseArgs()
 }
 
-func Start(cmd *cobra.Command, args []string) {
-	fmt.Println("Start")
+func ParseArgs() {
+	// TODO: Initialize Cobra (cli) and Viper (config)
+
+	// TODO: Cobra will call this...
+	StartNode(nil, nil)
+}
+
+func Initialize() {
+	// Setup initial logging
+	logger = log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+	logger.Info("Starting")
+}
+
+func StartNode(cmd *cobra.Command, args []string) {
+	fmt.Println("Starting up a Node")
+	node := app.NewApplicationContext()
+	service = server.NewGRPCServer("unix://data.sock", types.NewGRPCApplication(*node))
+	service.SetLogger(logger)
 }
