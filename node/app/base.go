1c5bf604301b9d50c9685f86c644e5000d1f3f38
Paul W. Homer <paul@oneledger.io>
2018-04-11 21:45:48 -0400

    Added logging and hooked up the App code.

--- node/app/base.go
+++ node/app/base.go
@@ -20,34 +20,43 @@ type responseInfo struct {
 	Txs    int `json:"txs"`
 }
 
-func (app *ApplicationContext) Info(req types.RequestInfo) types.ResponseInfo {
+func (app ApplicationContext) Info(req types.RequestInfo) types.ResponseInfo {
 	hashes := 0
 	txs := 0
 
 	bytes, _ := json.Marshal(&responseInfo{Hashes: hashes, Txs: txs})
 
-	// TODO: need to cast bytes into this type...
-	info := string(bytes)
-
-	return types.ResponseInfo{Data: info}
+	return types.ResponseInfo{Data: string(bytes)}
 }
 
-func (app *ApplicationContext) Query() types.ResponseQuery {
+func (app ApplicationContext) InitChain(req types.RequestInitChain) types.ResponseInitChain {
+	return types.ResponseInitChain{}
+}
+
+func (app ApplicationContext) Query(req types.RequestQuery) types.ResponseQuery {
 	return types.ResponseQuery{}
 }
 
-func (app *ApplicationContext) SetOption(req types.RequestSetOption) types.ResponseSetOption {
+func (app ApplicationContext) SetOption(req types.RequestSetOption) types.ResponseSetOption {
 	return types.ResponseSetOption{}
 }
 
-func (app *ApplicationContext) DeliverTx(tx []byte) types.ResponseDeliverTx {
-	return types.ResponseDeliverTx{}
-}
-
-func (app *ApplicationContext) CheckTx(tx []byte) types.ResponseCheckTx {
+func (app ApplicationContext) CheckTx(tx []byte) types.ResponseCheckTx {
 	return types.ResponseCheckTx{}
 }
 
-func (app *ApplicationContext) Commit() types.ResponseCommit {
+func (app ApplicationContext) BeginBlock(req types.RequestBeginBlock) types.ResponseBeginBlock {
+	return types.ResponseBeginBlock{}
+}
+
+func (app ApplicationContext) DeliverTx(tx []byte) types.ResponseDeliverTx {
+	return types.ResponseDeliverTx{}
+}
+
+func (app ApplicationContext) EndBlock(req types.RequestEndBlock) types.ResponseEndBlock {
+	return types.ResponseEndBlock{}
+}
+
+func (app ApplicationContext) Commit() types.ResponseCommit {
 	return types.ResponseCommit{}
 }
--- node/cmd/fullnode/main.go
+++ node/cmd/fullnode/main.go
@@ -1,3 +1,7 @@
+/*
+	Copyright 2017-2018 OneLedger
+*/
+
 package main
 
 import (
@@ -7,8 +11,11 @@ import (
 	"github.com/tendermint/abci/server"
 	"github.com/tendermint/abci/types"
 	"github.com/tendermint/tmlibs/common"
+	"github.com/tendermint/tmlibs/log"
+	"os"
 )
 
+// Setup a basic command for testing
 var start = &cobra.Command{
 	Run:   Start,
 	Use:   "start",
@@ -19,14 +26,23 @@ var start = &cobra.Command{
 var service common.Service
 
 func main() {
-	fmt.Println("Staring OneLedger Node")
+	//logger := log.TestingLogger()
+	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+
+	logger.Info("Starting")
+	//fmt.Println("Staring OneLedger Node")
+
+	// TODO: Initialize Cobra (cli) and Viper (config)
+
 	node := app.NewApplicationContext()
 	_ = node
 
 	// TODO: Just use a generic base, not hooked up yet.
-	app := types.NewBaseApplication()
+	//app := types.NewBaseApplication()
+	app := *node
 
 	service = server.NewGRPCServer("unix://data.sock", types.NewGRPCApplication(app))
+	service.SetLogger(logger)
 }
 
 func Start(cmd *cobra.Command, args []string) {
