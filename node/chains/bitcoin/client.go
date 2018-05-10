a87a199f013e114ea27693a0892b1efb77db0f50
alexjlan <jielan@oneledger.io>
2018-05-10 14:21:23 -0400

    add schedule fore block generation on regtest net

--- node/chains/bitcoin/client.go
+++ node/chains/bitcoin/client.go
@@ -2,9 +2,32 @@ package bitcoin
 
 import (
 	btc "github.com/Oneledger/prototype-api/btcrpc"
+	"time"
+
 )
 
-var btcClient, err = btc.New("127.0.0.1",8331, "olbtc", "olbtcpw01", true)
+//var btcClient, err = btc.New("127.0.0.1",18831, "oltest01", "olpass01", true)
 
 
+func ScheduleBlockGeneration(cli btc.Bitcoind, interval time.Duration ) chan bool {
+	ticker := time.NewTicker(interval * time.Second)
+	stop := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				//TODO add generate block rpc call wrapped
+				//call generate block for num
+			case <-stop:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+	return stop
+}
+
+func StopBlockGeneration(stop chan bool) {
+	close(stop)
+}
 
