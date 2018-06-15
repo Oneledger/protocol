c6887aba2f79bf9817b3fc144d6aee4b0000ca6d
alexjlan <jielan@oneledger.io>
2018-06-15 18:32:26 -0400

    modify btcclient connection

--- node/chains/bitcoin/client.go
+++ node/chains/bitcoin/client.go
@@ -6,14 +6,32 @@ import (
 	brpc "github.com/Oneledger/protocol/node/chains/bitcoin/rpc"
 
 	"github.com/Oneledger/protocol/node/log"
+	"strings"
+	"net"
+	"encoding/base64"
+	"strconv"
 )
 
-func GetBtcClient(rpcPort int) *brpc.Bitcoind {
+
+func GetBtcClient(address string) *brpc.Bitcoind {
+	addr:= strings.Split(address,":")
+	if len(addr) < 2 {
+		log.Error("address not in correct format")
+	}
+	ip := net.ParseIP(addr[0])
+	if ip == nil {
+		log.Error("address can not be parsed")
+	}
+
+	port, err := strconv.Atoi(addr[1])
+	if err != nil {
+		log.Error(err.Error())
+	}
 
 	usr, pass := getCredential()
-	cli, err := brpc.New("127.0.0.1", rpcPort, usr, pass, false)
+	cli, err :=  brpc.New(ip.String(), port, usr, pass, false)
 
-	if err != nil {
+	if err != nil{
 		log.Error(err.Error())
 		return nil
 	}
@@ -21,10 +39,20 @@ func GetBtcClient(rpcPort int) *brpc.Bitcoind {
 	return cli
 }
 
-func getCredential() (usr string, pass string) {
+func getCredential() (usr string, pass string){
 	//todo: getCredential from database which should be randomly generated when register or import if user already has bitcoin node
-	usr = "oltest01"
-	pass = "olpass01"
+	usrBytes, err := base64.StdEncoding.DecodeString("b2x0ZXN0MDE=")
+	if err != nil {
+		log.Error(err.Error())
+		usr = ""
+	}
+	usr = string(usrBytes)
+	passBytes, err := base64.StdEncoding.DecodeString("b2xwYXNzMDE=")
+	if err != nil {
+		log.Error(err.Error())
+		pass = ""
+	}
+	pass = string(passBytes)
 	return
 }
 
