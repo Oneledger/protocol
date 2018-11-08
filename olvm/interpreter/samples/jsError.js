a0493ff481200bd4b828113741ff9b1cd69152e1
Yang Li <stephen.liy@gmail.com>
2018-11-08 11:29:58 -0500

    Revert "Merge OLVM to the code base"
    This reverts commit ab905477d5b037916f50d358277666032683b964.

--- olvm/interpreter/samples/jsError.js
+++
@@ -1,9 +0,0 @@
-var jsErrorContract = function (context) {
-  this.context = context;
-}
-
-
-jsErrorContract.prototype.default__ = function () {
-  return undefine_value.length
-}
-module.Contract = jsErrorContract;
