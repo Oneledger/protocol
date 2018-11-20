a495c0a9b3b89a1aae3a50f3c531960c039c525f
pat-oneledger <pat@oneledger.io>
2018-11-20 16:03:46 -0400

    changed sequence from being stored in global.Current to database per accountKey

--- node/cmd/ollitenode/root.go
+++ node/cmd/ollitenode/root.go
@@ -45,9 +45,6 @@ func init() {
 
 	RootCmd.PersistentFlags().StringVarP(&global.Current.RpcAddress, "address", "a",
 		global.Current.RpcAddress, "full address")
-
-	RootCmd.PersistentFlags().Int64VarP(&global.Current.Sequence, "sequence", "s",
-		global.Current.Sequence, "unique sequence id")
 }
 
 // Initialize Viper
