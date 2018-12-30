8b8b19abb35a563f738ee6a324cbb8819e1063fe
pat-oneledger <pat@oneledger.io>
2018-12-30 11:39:34 -0400

    applyValidator only ran by individual nodes (1 VT) and admins (10 VTs)

--- node/app/query.go
+++ node/app/query.go
@@ -107,8 +107,6 @@ func HandleQuery(app Application, path string, arguments map[string]interface{})
 func HandleApplyValidatorQuery(application Application, arguments map[string]interface{}) interface{} {
 	log.Debug("HandleApplyValidatorQuery", "arguments", arguments)
 
-	conv := convert.NewConvert()
-
 	var argsHolder comm.ApplyValidatorArguments
 
 	text := arguments["parameters"].([]byte)
@@ -124,39 +122,69 @@ func HandleApplyValidatorQuery(application Application, arguments map[string]int
 	idName := args.(*comm.ApplyValidatorArguments).Id
 	purge := args.(*comm.ApplyValidatorArguments).Purge
 
+	nodeAccount := action.GetNodeAccount(application)
+	if nodeAccount == nil {
+		return "Node account is not found."
+	}
+
 	identity, ok := application.Identities.FindName(idName)
 	if ok != status.SUCCESS {
 		return "The identity is not found." + " id:" + idName
 	}
 
-	if amount == 0.0 {
-		return "Missing an amount argument"
+	nodeBalance := application.Balances.Get(data.DatabaseKey(nodeAccount.AccountKey()))
+
+	if &nodeBalance == nil {
+		return "Missing balance for node."
 	}
 
-	balance := application.Balances.Get(data.DatabaseKey(identity.AccountKey))
+	//check who is running the applyValidator command, if 10 VTs or more, then it's an administrator
+	tenVT := data.NewCoinFromFloat(10.0, "VT")
+	oneVT := data.NewCoinFromFloat(1.0, "VT")
+	stake := data.NewCoinFromFloat(amount, "VT")
 
-	if &balance == nil {
-		return "Missing Balance"
+	if nodeBalance.GetAmountByName("VT").LessThanCoin(tenVT) {
+		_, ok := application.Accounts.FindKey(identity.AccountKey)
+
+		if ok != status.SUCCESS {
+			return "Node account has less than 10 VTs. It can not run applyValidator for other identities."
+		}
+
+		//when removing validator, the node account doesn't have to have a VT
+		if purge == false {
+			if amount == 0.0 {
+				return "Missing an amount argument."
+			}
+
+			if nodeBalance.GetAmountByName("VT").LessThanCoin(stake) {
+				return "Validator Token is not enough."
+			}
+
+			if stake.LessThanCoin(oneVT) {
+				return "Validator Token amount can not be less than 1."
+			}
+		} else {
+			found := false
+			for _, validator := range application.Validators.Approved {
+				if validator.AccountKey.String() == nodeAccount.AccountKey().String() {
+					found = true
+				}
+			}
+			if found == false {
+				return "Node account is not a validator."
+			}
+		}
+
 	}
 
-	stake := conv.GetCoinFromFloat(amount, "VT")
-
-	if balance.GetAmountByName("VT").LessThanCoin(stake) {
-		return "Validator Token is not enough"
-	}
-
-	if stake.LessThanCurrency(1, "VT") {
-		return "Validator Token amount can not be less than 1"
-	}
-
-	sequence := SequenceNumber(application, identity.AccountKey.Bytes())
+	sequence := SequenceNumber(application, nodeAccount.AccountKey().Bytes())
 
 	validator := &action.ApplyValidator{
 		Base: action.Base{
 			Type:     action.APPLY_VALIDATOR,
 			ChainId:  ChainId,
-			Owner:    identity.AccountKey,
-			Signers:  GetSigners(identity.AccountKey, application),
+			Owner:    nodeAccount.AccountKey(),
+			Signers:  GetSigners(nodeAccount.AccountKey(), application),
 			Sequence: sequence.Sequence,
 		},
 
