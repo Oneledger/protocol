0a5de8890cecf5f1f87a2367543b53aa98872201
alexjlan <jielan@oneledger.io>
2019-01-18 17:14:15 -0500

    add test for continuous update the value of a key in chainstate

--- node/data/chainstate_test.go
+++ node/data/chainstate_test.go
@@ -44,7 +44,38 @@ func TestChainState(t *testing.T) {
 
 	state.Set(key, balance)
 	state.Commit()
-	result := state.Get(key)
+	result := state.Get(key, false)
 
-	assert.Equal(t, balance, *result, "These should be equal")
+	assert.Equal(t, balance, result, "These should be equal")
+}
+
+func TestChainStateContinueUpdate(t *testing.T) {
+
+	state := NewChainState("Continue", PERSISTENT)
+
+	key := make([]byte, 20)
+	key[0] = 0x03
+
+	balance := NewBalanceFromInt(10, "OLT")
+
+	state.Set(key, balance)
+
+	state.Commit()
+
+	//_, b1 := state.Delivered.ImmutableTree.Get(key)
+	b1 := state.Get(key, false)
+	log.Debug("with commit", "balance", b1)
+
+	assert.Equal(t, true, balance.GetAmountByName("OLT").Equals(b1.GetAmountByName("OLT")), "balance not eqaul after commit", b1)
+
+	newbalance := NewBalanceFromInt(14, "OLT")
+
+	state.Set(key, newbalance)
+
+	b2 := state.Get(key, false)
+	//_, b2 := state.Delivered.ImmutableTree.Get(key)
+	log.Debug("without commit", "balance", b2)
+
+	assert.Equal(t, true, newbalance.GetAmountByName("OLT").Equals(b2.GetAmountByName("OLT")), "balance not eqaul without commit", b2)
+
 }
