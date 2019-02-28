b96e3632b2a857ac04e1d057a9d7b1e32998a7f4
Jun Yan <jun.ying.yan@ryerson.ca>
2019-02-27 17:27:31 -0800

    add gcp vpc template

---
+++ gcp/main.tf
@@ -0,0 +1,38 @@
+provider "google-beta" {
+  alias = "chronos"
+  credentials = "${file("Chronos.json")}"
+  project     = "chronos-225820"
+  region      = "us-east1"
+  zone = "us-east1-b"
+}
+
+provider "google" {
+  alias = "devnet"
+  credentials = "${file("DevNet.json")}"
+  project     = "atomic-land-223022"
+  region      = "us-west1"
+  zone = "us-west1-b"
+}
+
+module "network"{
+  source = "./network"
+  subnet_cidr = "${var.subnet_cidr}"
+  name = "${var.name}"
+  providers = {
+    google = "google.devnet"
+  }
+}
+
+module "explorer"{
+  source = "./fullnode"
+  name = "${var.name}"
+  vpc_network = "${module.network.vpc_network}"
+  subnet = "${module.network.subnet}"
+  providers = {
+    google = "google.devnet"
+  }
+}
+
+output "public_ip" {
+  value = "${module.explorer.public_ip}"
+}
