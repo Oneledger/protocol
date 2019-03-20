dacbbfbd894ca1ca26daff8b9ce1da959c35f9fc
Enrico Baculinao <enrico@oneledger.io>
2019-03-20 15:55:06 -0400

    Add support for swapnoders in devnet command

--- node/cmd/olfullnode/devnet.go
+++ node/cmd/olfullnode/devnet.go
@@ -49,6 +49,7 @@ type testnetConfig struct {
 	numNonValidators int
 	outputDir        string
 	p2pPort          int
+	allowSwap        bool
 }
 
 var testnetArgs = &testnetConfig{}
@@ -65,6 +66,7 @@ func init() {
 	testnetCmd.Flags().IntVar(&testnetArgs.numValidators, "validators", 4, "Number of validators to initialize devnet with")
 	testnetCmd.Flags().IntVar(&testnetArgs.numNonValidators, "nonvalidators", 0, "Number of non-validators to initialize the devnet with")
 	testnetCmd.Flags().StringVarP(&testnetArgs.outputDir, "dir", "o", "./", "Directory to store initialization files for the devnet, default current folder")
+	testnetCmd.Flags().BoolVar(&testnetArgs.allowSwap, "enable-swaps", false, "Allow swaps")
 }
 
 // Need to maintain a list of nodes and be able to:
@@ -99,8 +101,17 @@ func portGenerator(startingPort int) func() int {
 	}
 }
 
-func generateAddress(port int) string {
-	return fmt.Sprintf("tcp://127.0.0.1:%d", port)
+func generateAddress(port int, hasProtocol bool) string {
+	var prefix string
+	ip := "127.0.0.1"
+	protocol := "tcp://"
+	if hasProtocol {
+		prefix = protocol + ip
+	} else {
+		prefix = ip
+	}
+
+	return fmt.Sprintf("%s:%d", prefix, port)
 }
 
 func runDevnet(cmd *cobra.Command, _ []string) error {
@@ -125,10 +136,10 @@ func runDevnet(cmd *cobra.Command, _ []string) error {
 		// Generate new configuration file
 		cfg := config.DefaultServerConfig()
 		cfg.Node.NodeName = nodeName
-		cfg.Network.RPCAddress = generateAddress(generatePort())
-		cfg.Network.P2PAddress = generateAddress(generatePort())
-		cfg.Network.SDKAddress = generateAddress(generatePort())
-		cfg.Network.OLVMAddress = generateAddress(generatePort())
+		cfg.Network.RPCAddress = generateAddress(generatePort(), true)
+		cfg.Network.P2PAddress = generateAddress(generatePort(), true)
+		cfg.Network.SDKAddress = generateAddress(generatePort(), true)
+		cfg.Network.OLVMAddress = generateAddress(generatePort(), true)
 
 		err := os.MkdirAll(configDir, config.DirPerms)
 		if err != nil {
@@ -182,8 +193,26 @@ func runDevnet(cmd *cobra.Command, _ []string) error {
 	}
 
 	// Save the files to the node's relevant directory
+	generateBTCPort := portGenerator(18831)
+	generateETHPort := portGenerator(28101)
+
+	swapNodes := nodeNames[1:4]
+	isSwapNode := func(name string) bool {
+		for _, nodeName := range swapNodes {
+			if nodeName == name {
+				return true
+			}
+		}
+		return false
+	}
 	for _, node := range nodeList {
 		node.cfg.P2P.PersistentPeers = persistentPeers
+		// Modify the btc and eth ports
+		if args.allowSwap && isSwapNode(node.cfg.Node.NodeName) {
+			node.cfg.Network.BTCAddress = generateAddress(generateBTCPort(), false)
+			node.cfg.Network.ETHAddress = generateAddress(generateETHPort(), false)
+		}
+
 		err := node.cfg.SaveFile(filepath.Join(node.dir, config.FileName))
 		if err != nil {
 			return err
