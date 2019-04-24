da0b472911f284ba38501ba308fbb7cb966d9d6c
Enrico Baculinao <enrico@oneledger.io>
2019-04-24 15:17:46 -0400

    Add Fatal and Fatalf

--- log/logger.go
+++ log/logger.go
@@ -3,6 +3,7 @@ package log
 import (
 	"fmt"
 	"io"
+	"strings"
 	"time"
 	kitlog "github.com/go-kit/kit/log"
 )
@@ -32,7 +33,9 @@ func (l Level) String() string {
 type Options struct {
 	// Set of levels to log
 	Prefix string
+	// Setting sync to true wraps the
 	Sync bool
+	// Defines a filter for each log filter
 	Levels map[Level]bool
 }
 
@@ -63,7 +66,7 @@ func NewLoggerWithOpts(w io.Writer, opts Options) *Logger {
 
 // WithPrefix returns a new logger with the prefix appended to the current logger's prefix
 func (l Logger) WithPrefix(prefix string) *Logger{
-	nextPrefix := l.prefix + " " + prefix
+	nextPrefix := strings.Trim(l.prefix + " " + prefix, " ")
 	return NewLoggerWithOpts(l.w, Options{
 		Prefix: nextPrefix,
 		Sync: false,
@@ -72,55 +75,65 @@ func (l Logger) WithPrefix(prefix string) *Logger{
 }
 
 func (l *Logger) Info(args...interface{}) {
-	l.fprintln(Info, args...)
+	l.fprintln(Info, time.Now(), args...)
 }
 
 func (l *Logger) Infof(format string, args...interface{}) {
-	l.fprintf(Info, format, args...)
+	l.fprintf(Info, time.Now(), format, args...)
 }
 
 func (l *Logger) Debug(args...interface{}) {
-	l.fprintln(Debug, args...)
+	l.fprintln(Debug, time.Now(), args...)
 }
 
 func (l *Logger) Debugf(format string, args...interface{}) {
-	l.fprintf(Debug, format, args...)
+	l.fprintf(Debug, time.Now(), format, args...)
 }
 
 func (l *Logger) Warning(args ...interface{}) {
-	l.fprintln(Warning, args...)
+	l.fprintln(Warning, time.Now(), args...)
 }
 
 func (l *Logger) Warningf(format string, args...interface{}) {
-	l.fprintf(Warning, format, args...)
+	l.fprintf(Warning, time.Now(), format, args...)
 }
 
 func (l *Logger) Error(args ...interface{}) {
-	l.fprintln(Error, args...)
+	l.fprintln(Error, time.Now(), args...)
 }
 
 func (l *Logger) Errorf(format string, args...interface{}) {
-	l.fprintf(Error, format, args...)
+	l.fprintf(Error, time.Now(), format, args...)
 }
 
-func (l *Logger) fprintln(level Level, args ...interface{}) {
+func (l *Logger) Fatal(args ...interface{}) {
+	msg := l.completePrefix(Fatal, time.Now()) + " " + fmt.Sprint(args...)
+	panic(msg)
+}
+
+func (l *Logger) Fatalf(format string, args...interface{}) {
+	 msg := l.completePrefix(Fatal, time.Now()) + " " + fmt.Sprintf(format, args...)
+	 panic(msg)
+}
+
+func (l *Logger) fprintln(level Level, now time.Time, args ...interface{}) {
 	if !l.levels[level] && level != Fatal {
 		return
 	}
 
-	args = append([]interface{}{l.completePrefix(level)}, args...)
+	args = append([]interface{}{l.completePrefix(level, now)}, args...)
 	_, err := fmt.Fprintln(l.w, args...)
 	if err != nil {
 		l.Error("LoggingError", err)
 	}
 }
 
-func (l *Logger) fprintf(level Level, format string, args ...interface{}) {
-	l.fprintln(level, fmt.Sprintf(format, args...))
+func (l *Logger) fprintf(level Level, now time.Time, format string, args ...interface{}) {
+	l.fprintln(level, now, fmt.Sprintf(format, args...))
 }
 
-func (l *Logger) completePrefix(level Level) string {
-	return fmt.Sprintf("%s[%s]%s", level, time.Now().Format(time.RFC3339), l.prefix)
+func (l *Logger) completePrefix(level Level, now time.Time) string {
+	return fmt.Sprintf("%s[%s]%s", level, now.Format(time.RFC3339), l.prefix)
 }
 
 // newSyncWriter takes an existing io.Writer and wraps a mutex around it to make it safe for use by concurrent goroutines
