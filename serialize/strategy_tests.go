a4ad5dcf70c799164e7d5d711fc7bc6d6dcd37e3
Harshad Patil <harshad@oneledger.io>
2019-04-29 11:07:07 -0400

    refactored some tests in serialize

---
+++ serialize/strategy_tests.go
@@ -0,0 +1,123 @@
+package serialize
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"github.com/vmihailenco/msgpack"
+	"testing"
+)
+
+type testStuff struct {
+	F string
+	A int
+	B int64
+	C []byte
+	H float64
+}
+
+var js = jsonStrategy{}
+
+func TestJsonStrategy_Serialize(t *testing.T) {
+	f := &testStuff{"asdf", 1, 123123, []byte("4983h4tsdof"), 56.77867342}
+	fb, err := js.Serialize(f)
+
+	assert.Nil(t, err)
+	eb, err := json.Marshal(f)
+	assert.Equal(t, eb, fb)
+
+	var fn = &testStuff{}
+	err = js.Deserialize(fb, fn)
+
+	assert.Nil(t, err)
+	assert.Equal(t, fn, f)
+}
+
+func TestJsonStrategy_SerializeForAdapter(t *testing.T) {
+
+	f := &testStuffAd{"asdf", 1, 123123, []byte("4983h4tsdof"), 56.77867342}
+	fb, err := js.Serialize(f)
+	assert.Nil(t, err)
+
+	temp := testStuffAdData{"asdf", 1, 123123, []byte("4983h4tsdof"), "56.77867342"}
+	eb, err := json.Marshal(temp)
+	assert.Nil(t, err)
+	assert.Equal(t, eb, fb)
+
+	var fn = &testStuffAd{}
+	err = js.Deserialize(fb, fn)
+
+	assert.Nil(t, err)
+	assert.Equal(t, fn, f)
+
+}
+
+
+
+var ms = msgpackStrategy{}
+
+func TestMsgpackStrategy_Serialize(t *testing.T) {
+	f := &testStuff{"asdf", 1, 123123, []byte("4983h4tsdof"), 56.77867342}
+	fb, err := ms.Serialize(f)
+
+	assert.Nil(t, err)
+	eb, err := msgpack.Marshal(f)
+	assert.Equal(t, eb, fb)
+
+	var fn = &testStuff{}
+	err = ms.Deserialize(fb, fn)
+
+	assert.Nil(t, err)
+	assert.Equal(t, fn, f)
+}
+
+func TestMsgpackStrategy_SerializeForAdapters(t *testing.T) {
+
+	f := &testStuffAd{"asdf", 1, 123123, []byte("4983h4tsdof"), 56.77867342}
+	fb, err := ms.Serialize(f)
+	assert.Nil(t, err)
+
+	temp := testStuffAdData{"asdf", 1, 123123, []byte("4983h4tsdof"), "56.77867342"}
+	eb, err := msgpack.Marshal(temp)
+	assert.Nil(t, err)
+	assert.Equal(t, eb, fb)
+
+	var fn = &testStuffAd{}
+	err = ms.Deserialize(fb, fn)
+
+	assert.Nil(t, err)
+	assert.Equal(t, fn, f)
+
+}
+
+
+
+type testStuff2 struct {
+	F string
+	A int
+	B int64
+	C []byte
+}
+
+var as *aminoStrategy
+
+func init() {
+	as = NewAminoStrategy(aminoCodec)
+}
+
+func TestAminoStrategy_Serialize(t *testing.T) {
+
+	RegisterConcrete(new(testStuff2), "testStuff2")
+	f := &testStuff2{"asdf", 1, 123123, []byte("4983h4tsdof")}
+	fb, err := as.Serialize(f)
+
+	assert.Nil(t, err)
+	eb, err := aminoCodec.MarshalBinaryLengthPrefixed(f)
+	assert.Equal(t, eb, fb)
+
+	var fn = &testStuff2{}
+	err = as.Deserialize(fb, fn)
+
+	assert.Nil(t, err)
+	assert.Equal(t, fn, f)
+}
+
