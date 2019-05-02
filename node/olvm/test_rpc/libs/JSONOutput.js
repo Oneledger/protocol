7e06bec17d38b83f9d40dc76ec68f62d043a9d42
alexjlan <jielan@oneledger.io>
2019-05-02 11:40:49 -0400

    remove node package

--- node/olvm/test_rpc/libs/JSONOutput.js
+++
@@ -1,9 +0,0 @@
-exports.jsonSend = (response,any) => {
-  response.setHeader('Content-Type', 'application/json');
-  response.send(JSON.stringify(any));
-}
-
-exports.jsonEnd = (response,any) => {
-  response.setHeader('Content-Type', 'application/json');
-  response.end(JSON.stringify(any));
-}
