8cc328f6b7133f8ee8df879aa251e407c9f70aba
steven <steven@oneledger.io>
2019-05-14 16:55:59 -0400

    init commit for  ansible load test script

--- gcp/load-test/variables.tf
+++ gcp/load-test/variables.tf
@@ -5,11 +5,14 @@ variable "gcp-vm-image" {
   default = "debian-cloud/debian-9"
 }
 variable "vpc_ip_range" {
-    default = "10.10.0.0/20"
+  default = "10.10.0.0/20"
 }
 variable "regions" {
   default = ["us-east-1", "us-east-4", "us-central1"]
 }
 variable "vmcount" {
   default = 5
+}
+variable "startup-script-url" {
+  default = ""
 }
