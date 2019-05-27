ea7acde5b861826acc54e89a363dc5a300afc7f2
Freddy Lee <tobeacookie@hotmail.com>
2019-05-27 11:31:48 -0400

    status file code format update

--- cmd/olfullnode/status.go
+++ cmd/olfullnode/status.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 	"net"
 	"net/url"
-	
-	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+
 	"github.com/Oneledger/protocol/config"
 	"github.com/Oneledger/protocol/log"
 	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
 
 	"github.com/kyokomi/emoji"
 )
@@ -18,13 +18,13 @@ import (
 var showStatusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "show node config file content",
-	RunE:  Init,
+	RunE:  initStatus,
 }
 
 type showStatusArgs struct {
-	logger       *log.Logger
-	cfg	         *config.Server
-	showConfig   bool
+	logger     *log.Logger
+	cfg        *config.Server
+	showConfig bool
 }
 
 var showStatusCtx = &showStatusArgs{}
@@ -35,7 +35,7 @@ func init() {
 	showStatusCmd.Flags().BoolVarP(&showStatusCtx.showConfig, "verbose", "v", false, "show the node config")
 }
 
-func Init(cmd *cobra.Command, args []string) error {
+func initStatus(cmd *cobra.Command, args []string) error {
 	ctx := showStatusCtx
 
 	rootPath, err := filepath.Abs(rootArgs.rootDir)
@@ -49,7 +49,7 @@ func Init(cmd *cobra.Command, args []string) error {
 		return errors.Wrapf(err, "failed to read configuration file at at %s", cfgPath(rootPath))
 	}
 
-	if ctx.showConfig {	
+	if ctx.showConfig {
 		err := ctx.dumpConfigContent(rootPath, cfg)
 		if err != nil {
 			return errors.Wrap(err, "failed to initialize config")
@@ -62,7 +62,7 @@ func Init(cmd *cobra.Command, args []string) error {
 
 func (ctx *showStatusArgs) dumpConfigContent(rootPath string, cfg *config.Server) error {
 	ctx.logger = log.NewLoggerWithPrefix(os.Stdout, "")
-	
+
 	ctx.logger.Dump("[Node]", cfg.Node)
 	ctx.logger.Dump("[Network]", cfg.Network)
 
@@ -70,49 +70,49 @@ func (ctx *showStatusArgs) dumpConfigContent(rootPath string, cfg *config.Server
 }
 
 func (ctx *showStatusArgs) checkNodes(rootPath string, cfg *config.Server) error {
-	urlRpc, err := url.Parse(cfg.Network.RPCAddress)
-	urlP2p, err := url.Parse(cfg.Network.P2PAddress)
-	urlSdk, err := url.Parse(cfg.Network.SDKAddress)
+	urlRPC, err := url.Parse(cfg.Network.RPCAddress)
+	urlP2P, err := url.Parse(cfg.Network.P2PAddress)
+	urlSDK, err := url.Parse(cfg.Network.SDKAddress)
 
 	if err != nil {
 		return errors.Wrapf(err, "failed to parse url")
 	}
-	
-	host, rpcPort, _ := net.SplitHostPort(urlRpc.Host)
-	host, p2pPort, _ := net.SplitHostPort(urlP2p.Host)
-	host, sdkPort, _ := net.SplitHostPort(urlSdk.Host)
-	
-	_, errRpc := net.Listen("tcp", host + ":" + rpcPort)
-    if errRpc != nil {
+
+	host, rpcPort, _ := net.SplitHostPort(urlRPC.Host)
+	host, p2pPort, _ := net.SplitHostPort(urlP2P.Host)
+	host, sdkPort, _ := net.SplitHostPort(urlSDK.Host)
+
+	_, errRPC := net.Listen("tcp", host+":"+rpcPort)
+	if errRPC != nil {
 		rpcTaken := emoji.Sprint("RPC Port: ", rpcPort, " on ", host, " :check_mark:")
 		fmt.Println(rpcTaken)
-    } else {
+	} else {
 		rpcAvail := emoji.Sprint("RPC Port: ", rpcPort, " on ", host, " :cross_mark:")
 		fmt.Println(rpcAvail)
 	}
 
-	_, errP2p := net.Listen("tcp", host + ":" + p2pPort)
-    if errP2p != nil {
-        p2pTaken := emoji.Sprint("P2P Port: ", p2pPort, " on ", host, " :check_mark:")
+	_, errP2P := net.Listen("tcp", host+":"+p2pPort)
+	if errP2P != nil {
+		p2pTaken := emoji.Sprint("P2P Port: ", p2pPort, " on ", host, " :check_mark:")
 		fmt.Println(p2pTaken)
-    } else {
+	} else {
 		p2pAvail := emoji.Sprint("P2P Port: ", p2pPort, " on ", host, " :cross_mark:")
 		fmt.Println(p2pAvail)
 	}
 
-	_, errSdk := net.Listen("tcp", host + ":" + sdkPort)
-    if errSdk != nil {
-        sdkTaken := emoji.Sprint("SDK Port: ", sdkPort, " on ", host, " :check_mark:")
+	_, errSDK := net.Listen("tcp", host+":"+sdkPort)
+	if errSDK != nil {
+		sdkTaken := emoji.Sprint("SDK Port: ", sdkPort, " on ", host, " :check_mark:")
 		fmt.Println(sdkTaken)
-    } else {
+	} else {
 		sdkAvail := emoji.Sprint("SDK Port: ", sdkPort, " on ", host, " :cross_mark:")
 		fmt.Println(sdkAvail)
 	}
-	if errRpc != nil && errP2p != nil && errSdk != nil {
+
+	if errRPC != nil && errP2P != nil && errSDK != nil {
 		allHealthy := emoji.Sprint(":clinking_beer_mugs::clinking_beer_mugs::clinking_beer_mugs: Looks all good :clinking_beer_mugs::clinking_beer_mugs::clinking_beer_mugs:")
 		fmt.Println(allHealthy)
 	}
 
-	return nil  
+	return nil
 }
-
