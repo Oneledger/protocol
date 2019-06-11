faccc1e6b060a69a1121e89fcea53829fbc4bcc2
Freddy Lee <tobeacookie@hotmail.com>
2019-06-11 17:27:27 -0400

    add generic hash function in utils

---
+++ utils/generic_hash.go
@@ -0,0 +1,24 @@
+/*
+   ____             _              _                      _____           _                  _
+  / __ \           | |            | |                    |  __ \         | |                | |
+ | |  | |_ __   ___| |     ___  __| | __ _  ___ _ __     | |__) | __ ___ | |_ ___   ___ ___ | |
+ | |  | | '_ \ / _ \ |    / _ \/ _` |/ _` |/ _ \ '__|    |  ___/ '__/ _ \| __/ _ \ / __/ _ \| |
+ | |__| | | | |  __/ |___|  __/ (_| | (_| |  __/ |       | |   | | | (_) | || (_) | (_| (_) | |
+  \____/|_| |_|\___|______\___|\__,_|\__, |\___|_|       |_|   |_|  \___/ \__\___/ \___\___/|_|
+                                      __/ |
+                                     |___/
+
+
+Copyright 2017 - 2019 OneLedger
+*/
+
+package utils
+
+import "golang.org/x/crypto/ripemd160"
+
+// Hash returns ripemd160 hash of the given input
+func Hash(result []byte) []byte {
+	hasher := ripemd160.New()
+	hasher.Write(result)
+	return hasher.Sum(nil)
+}
