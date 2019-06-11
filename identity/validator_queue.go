c2b6734803db4b76ed903e3e42b636ad480879af
Freddy Lee <tobeacookie@hotmail.com>
2019-06-11 12:06:37 -0400

    fix pointer issue in validator_queue

--- identity/validator_queue.go
+++ identity/validator_queue.go
@@ -9,26 +9,29 @@ type ValidatorQueue struct {
 	utils.PriorityQueue
 }
 
-func (vq ValidatorQueue) Pop() *utils.Queued {
+func (vq *ValidatorQueue) Pop() *utils.Queued {
+	if vq.Len() < 1 {
+		return nil
+	}
 	return heap.Pop(&vq.PriorityQueue).(*utils.Queued)
 }
 
-func (vq ValidatorQueue) Push(queued *utils.Queued) {
+func (vq *ValidatorQueue) Push(queued *utils.Queued) {
 	heap.Push(&vq.PriorityQueue, queued)
 }
 
-func (vq ValidatorQueue) Init() {
+func (vq *ValidatorQueue) Init() {
 	heap.Init(&vq.PriorityQueue)
 }
 
-func (vq ValidatorQueue) Len() int {
+func (vq *ValidatorQueue) Len() int {
 	return vq.PriorityQueue.Len()
 }
 
-func (vq ValidatorQueue) append(queued *utils.Queued) {
+func (vq *ValidatorQueue) append(queued *utils.Queued) {
 	vq.PriorityQueue = append(vq.PriorityQueue, queued)
 }
 
-func (vq ValidatorQueue) update(queued *utils.Queued, value []byte, priority int64) {
+func (vq *ValidatorQueue) update(queued *utils.Queued, value []byte, priority int64) {
 	vq.PriorityQueue.Update(queued, value, priority)
 }
