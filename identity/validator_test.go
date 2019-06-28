bbfb7f03ffa2fc172c8f37cec81e03e1f4fc1701
freddy <freddy@oneledger.io>
2019-06-28 16:04:43 -0400

    unit test for identity package

--- identity/validator_test.go
+++ identity/validator_test.go
@@ -13,12 +13,15 @@ import (
 var hexstring, _ = hex.DecodeString("89507C7ABC6D1E9124FE94101A0AB38D5085E15A")
 
 var v = &Validator{
-	Address:      []byte(hexstring),
-	StakeAddress: []byte(hexstring),
-	PubKey:       keys.PublicKey{0, []byte(hexstring)},
-	Power:        500,
-	Name:         "test node",
-	Staking:      balance.Coin{balance.Currency{"VT", 1, 18}, big.NewInt(100.0)},
+	Address:      hexstring,
+	StakeAddress: hexstring,
+	PubKey: keys.PublicKey{
+		KeyType: keys.ED25519,
+		Data:    nil,
+	},
+	Power:   500,
+	Name:    "test node",
+	Staking: balance.Coin{balance.Currency{"VT", 1, 18}, big.NewInt(100.0)},
 }
 
 func TestValidator_Bytes(t *testing.T) {
@@ -31,3 +34,13 @@ func TestValidator_FromBytes(t *testing.T) {
 		assert.Equal(t, v, validator)
 	}
 }
+
+func TestNewValidatorContext(t *testing.T) {
+	balance := &balance.Store{}
+	vc := &ValidatorContext{
+		Balances:balance,
+	}
+	ValidatorContext := NewValidatorContext(balance)
+	assert.Equal(t, ValidatorContext, vc)
+}
+
