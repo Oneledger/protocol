6cf6914b82c41be0447f92714f6d781e2916826c
Harschad Patil <harshad@oneledger.io>
2019-07-03 15:22:18 -0400

    starting pprof server on port 6060

--- cmd/olfullnode/main.go
+++ cmd/olfullnode/main.go
@@ -5,6 +5,17 @@
 */
 package main
 
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+)
+
 func main() {
+
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	Execute() // Pass control to Cobra
 }
