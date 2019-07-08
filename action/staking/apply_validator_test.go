7eb2f095701bd73940e0808b4de694ad4ca56a78
freddy <freddy@oneledger.io>
2019-07-08 17:20:39 -0400

    OLP-308 finish apply validator tx layer unit test

---
+++ action/staking/apply_validator_test.go
@@ -0,0 +1,340 @@
+package staking
+
+import (
+	"encoding/hex"
+	"errors"
+	"math/big"
+	"os"
+	"testing"
+
+	"github.com/Oneledger/protocol/config"
+	"github.com/Oneledger/protocol/identity"
+
+	"github.com/Oneledger/protocol/storage"
+
+	"github.com/Oneledger/protocol/data/balance"
+	"github.com/Oneledger/protocol/data/chain"
+
+	"github.com/tendermint/tendermint/crypto"
+
+	"github.com/tendermint/tendermint/crypto/ed25519"
+
+	"github.com/Oneledger/protocol/action"
+
+	"github.com/Oneledger/protocol/data/keys"
+	"github.com/stretchr/testify/assert"
+)
+
+// global setup
+func setup() []string {
+	testDBs := []string{"test_dbpath"}
+	return testDBs
+}
+
+// remove test db dir after
+func teardown(dbPaths []string) {
+	for _, v := range dbPaths {
+		err := os.RemoveAll(v)
+		if err != nil {
+			errors.New("Remove test db file error")
+		}
+	}
+}
+
+func generateKeyPair() (crypto.Address, crypto.PubKey, ed25519.PrivKeyEd25519) {
+
+	prikey := ed25519.GenPrivKey()
+	pubkey := prikey.PubKey()
+	addr := pubkey.Address()
+
+	return addr, pubkey, prikey
+}
+
+func assemblyData(addr crypto.Address, pubkey crypto.PubKey, prikey ed25519.PrivKeyEd25519, stake action.Amount, purge bool) *action.BaseTx {
+
+	av := &ApplyValidator{
+		Address:          addr.Bytes(),
+		Stake:            stake,
+		NodeName:         "test_node",
+		ValidatorAddress: addr.Bytes(),
+		ValidatorPubKey:  keys.PublicKey{keys.ED25519, pubkey.Bytes()[5:]},
+		Purge:            purge,
+	}
+	fee := action.Fee{
+		Price: action.Amount{"OLT", "10"},
+		Gas:   int64(10),
+	}
+	tx := &action.BaseTx{
+		Data: av,
+		Fee:  fee,
+		Memo: "test_memo",
+	}
+	signature, _ := prikey.Sign(tx.Bytes())
+	signer := action.Signature{
+		Signer: keys.PublicKey{keys.ED25519, pubkey.Bytes()[5:]},
+		Signed: signature,
+	}
+	tx.Signatures = []action.Signature{signer}
+
+	return tx
+}
+
+func setupForApplyValidatorWithPurge() *action.BaseTx {
+
+	addr, pubkey, prikey := generateKeyPair()
+
+	stake := action.Amount{"VT", "0.01"}
+	tx := assemblyData(addr, pubkey, prikey, stake, true)
+
+	return tx
+}
+
+func setupForApplyValidator() *action.BaseTx {
+
+	addr, pubkey, prikey := generateKeyPair()
+
+	stake := action.Amount{"VT", "0.01"}
+	tx := assemblyData(addr, pubkey, prikey, stake, false)
+
+	return tx
+}
+
+func setupForInvalidStakeAmount() *action.BaseTx {
+
+	addr, pubkey, prikey := generateKeyPair()
+
+	stake := action.Amount{"OLT", "-10"}
+	tx := assemblyData(addr, pubkey, prikey, stake, false)
+
+	return tx
+}
+
+func setupForTypeCastApplyValidator() *action.BaseTx {
+
+	addr, pubkey, prikey := generateKeyPair()
+
+	// test cast for type casting error
+	av := ApplyValidator{
+		Address:          addr.Bytes(),
+		Stake:            action.Amount{"OLT", "10"},
+		NodeName:         "test_node",
+		ValidatorAddress: addr.Bytes(),
+		ValidatorPubKey:  keys.PublicKey{keys.ED25519, pubkey.Bytes()[5:]},
+		Purge:            false,
+	}
+	fee := action.Fee{
+		Price: action.Amount{"OLT", "10"},
+		Gas:   int64(10),
+	}
+	tx := &action.BaseTx{
+		Data: av,
+		Fee:  fee,
+		Memo: "test_memo",
+	}
+	signature, _ := prikey.Sign(tx.Bytes())
+	signer := action.Signature{
+		Signer: keys.PublicKey{keys.ED25519, pubkey.Bytes()[5:]},
+		Signed: signature,
+	}
+	tx.Signatures = []action.Signature{signer}
+
+	return tx
+}
+
+func TestApplyValidator_Signers(t *testing.T) {
+	t.Run("run empty Signer test case, shoule return an empty result", func(t *testing.T) {
+		apply := ApplyValidator{}
+		correctResult := []action.Address{nil}
+		assert.Equal(t, correctResult, apply.Signers())
+	})
+	t.Run("run random Signer test case, should return the same result as the input data", func(t *testing.T) {
+		hexData, err := hex.DecodeString("89507C7ABC6D1E9124FE94101A0AB38D5085E15A")
+		assert.Nil(t, err)
+		b := []byte(hexData)
+		correctResult := []action.Address{b}
+		apply := ApplyValidator{}
+		apply.Address = keys.Address(b).Bytes()
+		assert.Equal(t, correctResult, apply.Signers())
+	})
+}
+
+func TestApplyTx_Validate(t *testing.T) {
+	t.Run("test validator basic part, should return no error but validator basic check should be false", func(t *testing.T) {
+		tx := setupForApplyValidator()
+
+		signer := keys.PublicKey{
+			KeyType: keys.ED25519,
+			Data:    []byte(""),
+		}
+		signature := action.Signature{
+			Signer: signer,
+			Signed: []byte(""),
+		}
+		signatures := []action.Signature{signature}
+		ctx := &action.Context{}
+		handler := applyTx{}
+		ok, err := handler.Validate(ctx, tx.Data, tx.Fee, tx.Memo, signatures)
+		assert.False(t, ok)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("test apply validator type cast part, should be not ok", func(t *testing.T) {
+		tx := setupForTypeCastApplyValidator()
+		ctx := &action.Context{}
+		handler := applyTx{}
+		ok, err := handler.Validate(ctx, tx.Data, tx.Fee, tx.Memo, tx.Signatures)
+		assert.False(t, ok)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("test apply validator with an invalid currency type, should be not ok", func(t *testing.T) {
+		tx := setupForInvalidStakeAmount()
+
+		currencyList := balance.NewCurrencyList()
+		currency := balance.Currency{
+			Name:    "OLT",
+			Chain:   chain.Type(1),
+			Decimal: int64(18),
+		}
+		err := currencyList.Register(currency)
+		assert.Nil(t, err, "register new OLT token should be ok")
+		ctx := &action.Context{
+			Currencies: currencyList,
+		}
+		handler := applyTx{}
+		ok, err := handler.Validate(ctx, tx.Data, tx.Fee, tx.Memo, tx.Signatures)
+		assert.False(t, ok)
+		assert.NotNil(t, err)
+	})
+	
+	t.Run("test apply validator with an invalid currency type, should be not ok", func(t *testing.T) {
+		tx := setupForApplyValidator()
+
+		currencyList := balance.NewCurrencyList()
+		currency := balance.Currency{
+			Name:    "OLT",
+			Chain:   chain.Type(1),
+			Decimal: int64(18),
+		}
+		err := currencyList.Register(currency)
+		assert.Nil(t, err, "register new OLT token should be ok")
+		ctx := &action.Context{
+			Currencies: currencyList,
+		}
+		handler := applyTx{}
+		ok, err := handler.Validate(ctx, tx.Data, tx.Fee, tx.Memo, tx.Signatures)
+		assert.False(t, ok)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("test apply validator with valid tx data, should be ok", func(t *testing.T) {
+		tx := setupForApplyValidator()
+		currencyList := balance.NewCurrencyList()
+		currency := balance.Currency{
+			Name:    "VT",
+			Chain:   chain.Type(1),
+			Decimal: int64(18),
+		}
+		err := currencyList.Register(currency)
+		assert.Nil(t, err, "register new currency should be ok")
+		ctx := &action.Context{
+			Currencies: currencyList,
+		}
+		handler := applyTx{}
+		ok, err := handler.Validate(ctx, tx.Data, tx.Fee, tx.Memo, tx.Signatures)
+		assert.True(t, ok)
+		assert.Nil(t, err)
+	})
+}
+
+func TestApplyTx_ProcessCheck(t *testing.T) {
+	t.Run("cast applyvalidator tx with invalid data, should return error", func(t *testing.T) {
+		tx := setupForTypeCastApplyValidator()
+		ctx := &action.Context{}
+		handler := applyTx{}
+		ok, _ := handler.ProcessCheck(ctx, tx.Data, tx.Fee)
+		assert.False(t, ok)
+	})
+	t.Run("check balance with valid data, should return no error", func(t *testing.T) {
+		testDB := setup()
+		defer teardown(testDB)
+		tx := setupForApplyValidator()
+
+		store := balance.NewStore("test_balances", "test_dbpath", storage.CACHE, storage.PERSISTENT)
+		ctx := &action.Context{
+			Balances: store,
+		}
+		handler := applyTx{}
+
+		ok, _ := handler.ProcessCheck(ctx, tx.Data, tx.Fee)
+		assert.False(t, ok)
+	})
+}
+
+func TestApplyTx_ProcessDeliver(t *testing.T) {
+	t.Run(" process with valid data, should return no error", func(t *testing.T) {
+		testDB := setup()
+		defer teardown(testDB)
+		tx := setupForApplyValidator()
+
+		store := balance.NewStore("test_balances", "test_dbpath", storage.CACHE, storage.PERSISTENT)
+		cfg := config.Server{}
+		validator := identity.NewValidatorStore(cfg, "test_dbpath", "test_db_type")
+		currencyList := balance.NewCurrencyList()
+		currency := balance.Currency{
+			Name:    "VT",
+			Chain:   chain.Type(1),
+			Decimal: int64(18),
+		}
+		coin := balance.Coin{
+			Currency: currency,
+			Amount:   big.NewInt(100000000000000000),
+		}
+		ba := balance.NewBalance()
+		ba = ba.AddCoin(coin)
+		err := store.Set(tx.Data.Signers()[0], *ba)
+		assert.Nil(t, err, "set some VT token for test, should be ok")
+		err = currencyList.Register(currency)
+		assert.Nil(t, err, "register new currency should be ok")
+		ctx := &action.Context{
+			Balances:   store,
+			Validators: validator,
+			Currencies: currencyList,
+		}
+		handler := applyTx{}
+		ok, _ := handler.ProcessDeliver(ctx, tx.Data, tx.Fee)
+		assert.True(t, ok)
+	})
+	t.Run("process with valid data but using purge flag, should return no error", func(t *testing.T) {
+		testDB := setup()
+		defer teardown(testDB)
+		tx := setupForApplyValidatorWithPurge()
+		store := balance.NewStore("test_balances", "test_dbpath", storage.CACHE, storage.PERSISTENT)
+		cfg := config.Server{}
+		validator := identity.NewValidatorStore(cfg, "test_dbpath", "test_db_type")
+		currencyList := balance.NewCurrencyList()
+		currency := balance.Currency{
+			Name:    "VT",
+			Chain:   chain.Type(1),
+			Decimal: int64(18),
+		}
+		coin := balance.Coin{
+			Currency: currency,
+			Amount:   big.NewInt(100000000000000000),
+		}
+		ba := balance.NewBalance()
+		ba = ba.AddCoin(coin)
+		err := store.Set(tx.Data.Signers()[0], *ba)
+		assert.Nil(t, err, "set some VT token for test, should be ok")
+		err = currencyList.Register(currency)
+		assert.Nil(t, err, "register new currency should be ok")
+		ctx := &action.Context{
+			Balances:   store,
+			Validators: validator,
+			Currencies: currencyList,
+		}
+		handler := applyTx{}
+		ok, _ := handler.ProcessDeliver(ctx, tx.Data, tx.Fee)
+		assert.False(t, ok)
+	})
+}
