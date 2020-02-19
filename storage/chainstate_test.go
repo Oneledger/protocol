7c36219d4b96ded70b62158453c803bd27992527
alexjlan <jielan@oneledger.io>
2020-02-18 21:00:02 -0500

    add state test to capture apphash problem

--- storage/chainstate_test.go
+++ storage/chainstate_test.go
@@ -19,12 +19,19 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/tendermint/tendermint/libs/db"
 )
+
+var cacheDB db.DB
+
+func init() {
+	cacheDB = db.NewDB("test", db.MemDBBackend, "")
+}
 
 func TestPersistence(t *testing.T) {
 	log.Debug("Create new chain state")
 
-	state := NewChainState("PersistentTest", "./", "goleveldb", PERSISTENT)
+	state := NewChainState("PersistentTest", cacheDB)
 
 	key := "Hello"
 	value := "The Value"
@@ -46,7 +53,7 @@ func TestPersistence(t *testing.T) {
 }
 
 func TestChainState(t *testing.T) {
-	state := NewChainState("ChainState", "./", "goleveldb", PERSISTENT)
+	state := NewChainState("ChainState", cacheDB)
 	key := make([]byte, 20)
 	key[0] = 0xaf
 	value := []byte("value1")
@@ -54,14 +61,14 @@ func TestChainState(t *testing.T) {
 	err := state.Set(key, value)
 	assert.Nil(t, err)
 	state.Commit()
-	result := state.Get(key, false)
-
+	result, err := state.Get(key)
+	assert.NoError(t, err, "get")
 	assert.Equal(t, value, result, "These should be equal")
 }
 
 func TestChainStateContinueUpdate(t *testing.T) {
 
-	state := NewChainState("Continue", "./", "goleveldb", PERSISTENT)
+	state := NewChainState("Continue", cacheDB)
 
 	key := make([]byte, 20)
 	key[0] = 0x03
@@ -74,8 +81,8 @@ func TestChainStateContinueUpdate(t *testing.T) {
 	state.Commit()
 
 	//_, b1 := state.Delivered.ImmutableTree.Get(key)
-	r1 := state.Get(key, false)
-
+	r1, err := state.Get(key)
+	assert.NoError(t, err)
 	assert.Equal(t, value, r1, "value not eqaul after commit")
 
 	value2 := []byte("value2")
@@ -83,15 +90,15 @@ func TestChainStateContinueUpdate(t *testing.T) {
 	err = state.Set(key, value2)
 	assert.Nil(t, err)
 
-	r2 := state.Get(key, false)
-
+	r2, err := state.Get(key)
+	assert.NoError(t, err)
 	assert.Equal(t, value2, r2, "value not eqaul without commit")
 
 }
 
 func TestChainState_Commit(t *testing.T) {
 
-	state := NewChainState("commit", "./", "goleveldb", PERSISTENT)
+	state := NewChainState("commit", cacheDB)
 
 	key := make([]byte, 20)
 	key[0] = 0x05
@@ -101,15 +108,10 @@ func TestChainState_Commit(t *testing.T) {
 
 	hash, version := state.Commit()
 
-	// Force the database to completely close, then repoen it.
-	state.database.Close()
-	state.database = nil
-
-	// Update all of the chain parameters
-	nhash, nversion := state.reset()
+	nhash, nversion := state.Commit()
 
 	// Check the reset
 	assert.Equal(t, 0, bytes.Compare(hash, nhash), "hash of persistent after commit not match")
 
-	assert.Equal(t, version, nversion, "version of persistent after commit not match")
+	assert.Equal(t, version+1, nversion, "version of persistent after commit not match")
 }
