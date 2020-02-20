0db2f64789c80996d5410c3d96d1817c0eeea3a8
KevinOneLedger <kevin@oneledger.io>
2020-02-20 12:48:38 -0500

    OLP-646: Defer Wait.Done() call when termination signal caught.

--- cmd/olfullnode/node.go
+++ cmd/olfullnode/node.go
@@ -152,12 +152,19 @@ func catchSigTerm(logger *log.Logger, close func(), waiter *sync.WaitGroup) {
 	signal.Notify(sigs, os.Interrupt, os.Kill, syscall.SIGSTOP, syscall.SIGTERM, syscall.SIGQUIT)
 
 	go func() {
+		sigCaught := false
+
 		for sig := range sigs {
 			logger.Info("Stopping due to", sig.String())
+			sigCaught = true
 			close()
-
-			waiter.Done()
 		}
+
+		defer func(sig bool) {
+			if sig {
+				waiter.Done()
+			}
+		}(sigCaught)
 	}()
 
 }
