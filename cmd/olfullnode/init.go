cec27eead7b19acc110219140a48adc4636de9ce
alexjlan <jielan@oneledger.io>
2020-04-12 22:33:38 -0400

    change the olfullnode init command to generate keys without genesis file

--- cmd/olfullnode/init.go
+++ cmd/olfullnode/init.go
@@ -51,53 +51,27 @@ type initContext struct {
 	nodeName string
 }
 
-func newInitContext(args *InitCmdArguments, rootArgs *rootArguments) (*initContext, error) {
-	logger := log.NewLoggerWithPrefix(os.Stdout, "olfullnode init")
-
-	rootDir, err := filepath.Abs(rootArgs.rootDir)
-	if err != nil {
-		return nil, errors.Wrap(err, fmt.Sprintf("Invalid root directory specified %s", rootArgs))
-	}
-
-	genesisPath, err := filepath.Abs(args.genesis)
-	if err != nil {
-		return nil, err
-	}
-	genesis, err := types.GenesisDocFromFile(genesisPath)
-	if err != nil {
-		return nil, err
-	}
-
-	return &initContext{
-		rootDir: rootDir,
-		logger:  logger,
-		genesis: genesis,
-	}, nil
-}
-
-func runInitNode(cmd *cobra.Command, _ []string) error {
-	ctx, err := newInitContext(initCmdArgs, rootArgs)
-	if err != nil {
-		return err
-	}
-	return initNode(ctx)
-}
-
 // Given the path of a genesis file and a specified root directory, initNode creates all the configuration files
 // needed to run a fullnode inside that specified directory
-func initNode(ctx *initContext) error {
-	if _, err := os.Stat(ctx.rootDir); os.IsNotExist(err) {
-		err = os.Mkdir(ctx.rootDir, config.DirPerms)
+func runInitNode(cmd *cobra.Command, _ []string) error {
+	rootDir, err := filepath.Abs(rootArgs.rootDir)
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("Invalid root directory specified %s", rootArgs))
+	}
+
+	if _, err := os.Stat(rootDir); os.IsNotExist(err) {
+		err = os.Mkdir(rootDir, config.DirPerms)
 		if err != nil {
-			return errors.Wrap(err, fmt.Sprintf("Failed to create the specified rootdir at %s", ctx.rootDir))
+			return errors.Wrap(err, fmt.Sprintf("Failed to create the specified rootdir at %s", rootDir))
 		}
 	}
+	logger := log.NewLoggerWithPrefix(os.Stdout, "olfullnode init")
 
 	// Generate new configuration file
 	cfg := config.DefaultServerConfig()
-	cfg.Node.NodeName = ctx.nodeName
-	cfgPath := filepath.Join(ctx.rootDir, config.FileName)
-	err := cfg.SaveFile(cfgPath)
+	cfg.Node.NodeName = initCmdArgs.nodeName
+	cfgPath := filepath.Join(rootDir, config.FileName)
+	err = cfg.SaveFile(cfgPath)
 	if err != nil {
 		return errors.Wrap(err, fmt.Sprintf("Failed to create config file at %s", cfgPath))
 	}
@@ -106,9 +80,9 @@ func initNode(ctx *initContext) error {
 	csConfig := consensus.ConfigDirName
 	csData := consensus.DataDirName
 
-	configDir := filepath.Join(ctx.rootDir, csRoot, csConfig)
-	dataDir := filepath.Join(ctx.rootDir, csRoot, csData)
-	nodeDataDir := filepath.Join(ctx.rootDir, "nodedata")
+	configDir := filepath.Join(rootDir, csRoot, csConfig)
+	dataDir := filepath.Join(rootDir, csRoot, csData)
+	nodeDataDir := filepath.Join(rootDir, "nodedata")
 
 	dirs := []string{configDir, dataDir, nodeDataDir}
 	for _, dir := range dirs {
@@ -119,16 +93,31 @@ func initNode(ctx *initContext) error {
 	}
 
 	// Put the genesis file in the right place
-	err = ctx.genesis.SaveAs(filepath.Join(configDir, consensus.GenesisFilename))
-	if err != nil {
-		return errors.Wrap(err, "Failed to save genesis file")
+	if initCmdArgs.genesis != "" {
+
+		genesisPath, err := filepath.Abs(initCmdArgs.genesis)
+		if err != nil {
+			return errors.Wrap(err, "invalid genesis file path")
+		}
+		logger.Info("verifying genesis file provided")
+		genesis, err := types.GenesisDocFromFile(genesisPath)
+		if err != nil {
+			return err
+		}
+		err = genesis.SaveAs(filepath.Join(configDir, consensus.GenesisFilename))
+		if err != nil {
+			return errors.Wrap(err, "Failed to save genesis file")
+		}
+		// Make node key
+
+	} else {
+		logger.Info("no genesis file provided, node is not runnable until genesis file is provided at: ", filepath.Join(configDir, consensus.GenesisFilename))
 	}
-	// Make node key
+
 	_, err = p2p.LoadOrGenNodeKey(filepath.Join(configDir, consensus.NodeKeyFilename))
 	if err != nil {
 		return errors.Wrap(err, "Failed to generate node key")
 	}
-
 	// Make private validator file
 	pvFile := privval.GenFilePV(filepath.Join(configDir, consensus.PrivValidatorKeyFilename),
 		filepath.Join(dataDir, consensus.PrivValidatorStateFilename))
