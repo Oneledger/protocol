a1cc9180fce2dc80a120ae36d951108a849361fa
alexjlan <jielan@oneledger.io>
2020-04-23 11:41:23 -0400

    add cleanup scrpts

---
+++ DOCKER/kainos-test/clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pkill olfullnode
+
+rm *.log
+rm -rf nodedata/*
+rm -rf consensus/data/*.db
+rm -rf consensus/data/cs.wal
+rm -rf consensus/config/write*
+rm -rf consensus/config/genesis.json
+wget https://github.com/Oneledger/protocol/blob/develop/DOCKER/kainos-test/genesis.json
+
+mv genesis.json ./consensus/config/
+echo '{
+  "height": "0",
+  "round": "0",
+  "step": 0,
+}'  > ./consensus/data/priv_validator_state.json
+
