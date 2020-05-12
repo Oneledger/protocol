7abb00dd870c1a65e495158900149d2133280bb0
KevinOneLedger <kevin@oneledger.io>
2020-05-12 12:21:16 -0400

    Removed iota from constant types.

--- data/governance/init.go
+++ data/governance/init.go
@@ -1,24 +1,28 @@
 package governance
 
 const (
-	ProposalTypeConfigUpdate ProposalType = iota
-	ProposalTypeCodeChange
-	ProposalTypeGeneral
+	//Proposal Types
+	ProposalTypeConfigUpdate ProposalType = 0x20
+	ProposalTypeCodeChange   ProposalType = 0x21
+	ProposalTypeGeneral      ProposalType = 0x22
 
-	ProposalStatusFunding ProposalStatus = iota
-	ProposalStatusVoting
-	ProposalStatusCompleted
+	//Proposal Status
+	ProposalStatusFunding   ProposalStatus = 0x23
+	ProposalStatusVoting    ProposalStatus = 0x24
+	ProposalStatusCompleted ProposalStatus = 0x25
 
-	ProposalOutcomeInProgress ProposalOutcome = iota
-	ProposalOutcomeInsufficientFunds
-	ProposalOutcomeInsufficientVotes
-	ProposalOutcomeCancelled
-	ProposalOutcomeCompleted
+	//Proposal Outcome
+	ProposalOutcomeInProgress        ProposalOutcome = 0x26
+	ProposalOutcomeInsufficientFunds ProposalOutcome = 0x27
+	ProposalOutcomeInsufficientVotes ProposalOutcome = 0x28
+	ProposalOutcomeCancelled         ProposalOutcome = 0x29
+	ProposalOutcomeCompleted         ProposalOutcome = 0x30
 
-	ProposalStateError  ProposalState = -1
-	ProposalStateActive ProposalState = iota
-	ProposalStatePassed
-	ProposalStateFailed
+	//Proposal States
+	ProposalStateError  ProposalState = 0xEE
+	ProposalStateActive ProposalState = 0x31
+	ProposalStatePassed ProposalState = 0x32
+	ProposalStateFailed ProposalState = 0x33
 
 	//Error Codes
 	errorSerialization   = "321"
