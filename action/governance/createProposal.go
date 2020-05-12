12cfae4bc2d31bb1f370fa00d2ac9ce323df833d
KevinOneLedger <kevin@oneledger.io>
2020-05-12 14:26:29 -0400

    implement validate and create proposal request.

--- action/governance/createProposal.go
+++ action/governance/createProposal.go
@@ -2,16 +2,45 @@ package governance
 
 import (
 	"github.com/Oneledger/protocol/action"
+	"github.com/Oneledger/protocol/data/keys"
+	"github.com/pkg/errors"
 	"github.com/tendermint/tendermint/libs/kv"
 )
 
 var _ action.Msg = &CreateProposal{}
 
 type CreateProposal struct {
+	proposalType    int //TODO: Change types to governance types
+	description     string
+	proposer        keys.Address
+	initialFunding  int
+	fundingDeadline int
+	fundingGoal     int
+	votingDeadline  int
 }
 
 func (c CreateProposal) Validate(ctx *action.Context, signedTx action.SignedTx) (bool, error) {
-	panic("implement me")
+	createProposal := CreateProposal{}
+	err := createProposal.Unmarshal(signedTx.Data)
+	if err != nil {
+		return false, errors.Wrap(action.ErrWrongTxType, err.Error())
+	}
+
+	//validate basic signature
+	err = action.ValidateBasic(signedTx.RawBytes(), createProposal.Signers(), signedTx.Signatures)
+	if err != nil {
+		return false, err
+	}
+
+	err = action.ValidateFee(ctx.FeePool.GetOpt(), signedTx.Fee)
+	if err != nil {
+		return false, err
+	}
+
+	//validate transaction specific field
+	//Check if initial funding is greater than minimum amount based on type.
+
+	return true, nil
 }
 
 func (c CreateProposal) ProcessCheck(ctx *action.Context, tx action.RawTx) (bool, action.Response) {
