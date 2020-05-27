426c446ce583386158efd5824c41ec7dc2c1b3c8
Hao <haosun@oneledger.com>
2020-05-27 16:46:03 -0400

    fix and integration test

--- scripts/governance/withdrawFunds.py
+++ scripts/governance/withdrawFunds.py
@@ -3,27 +3,41 @@ import time
 from sdk.actions import *
 
 addr_list = addresses()
+addr_list_1 = addresses_from_second_node()
 
-_pid = "id_30010"
+_pid = "id_30025"
 _proposer = addr_list[0]
-_initial_funding = (int("10023450") * 10 ** 14)
-_contributor = addr_list[1]
+_initial_funding = (int("2") * 10 ** 9)
+_contributor = addr_list_1[0]
+_funds_amount = (int("2") * 10 ** 5)
 
-_prop = Proposal(_pid, "general", "proposal for fund withdraw", _proposer, _initial_funding)
-_prop_fund = ProposalFund(_pid, 100, _contributor)
-_prop_fund_withdraw = ProposalFundsWithdraw(_pid, _contributor, 100, _contributor)
+_prop = Proposal(_pid, "general", "proposal for funds withdrawing", _proposer, _initial_funding)
+_encoded_pid = _prop.get_encoded_pid()
+_prop_fund = ProposalFund(_pid, _funds_amount, _contributor)
+_prop_fund_withdraw_initial_funding = ProposalFundsWithdraw(_encoded_pid, _proposer, _funds_amount, _proposer)
+_prop_fund_withdraw = ProposalFundsWithdraw(_encoded_pid, _contributor, _funds_amount, _contributor)
+
+_wait = 6
 
 if __name__ == "__main__":
+    print bcolors.WARNING + "*** Start testing funds withdraw ***" + bcolors.ENDC
     # create proposal
     _prop.send_create()
-    time.sleep(5)
 
     # fund proposal
     _prop_fund.send_fund()
-    time.sleep(20)
+    time.sleep(5)
+
+    for x in range(_wait):
+        print("wait for 60s, " + str(_wait * 10 - x * 10) + "s left")
+        time.sleep(10)
+
+    # withdraw proposal funds---withdraw initial fund
+    _prop_fund_withdraw_initial_funding.withdraw_fund()
+    time.sleep(5)
 
     # withdraw proposal funds
-    _prop_fund_withdraw.withdraw_fund()
+    _prop_fund_withdraw_initial_funding.withdraw_fund()
     time.sleep(5)
 
     print "#### ACTIVE PROPOSALS: ####"
