9e13add041616518576a97adbbb42f4102fed4e1
manikanta <manikanta4a6@gmail.com>
2020-06-05 13:05:32 -0400

    adding test-script

---
+++ test_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd protocol
+
+# to build binary
+make install_c
+
+# unit testing
+make utest
+
+# apply validator test
+make applytest
+
+# ONS test, oneledger naming service tests
+make onstest
+
+# withdraw test
+make withdrawtest
+
+# governance test
+make govtest
+
+# all tests
+make alltest
+
+# make rpc auth test
+make rpcAuthtest
+
+# generate coverage result, cover.html under protocol
+make coverage
