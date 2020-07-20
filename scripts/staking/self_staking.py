df31e2c318e0ce5d8058f7589756c6dba4b7fca7
ws4charlie <charlie@oneledger.io>
2020-07-20 00:52:19 -0400

    fixed integration test failures

--- scripts/staking/self_staking.py
+++ scripts/staking/self_staking.py
@@ -9,20 +9,20 @@ if __name__ == "__main__":
     staking_test.prepare()
     addr_list = addresses()
 
-    staking_test.stake('3', addr_list[1], True)
-    staking_test.checkStatus(addr_list[1], 3, 0, False)
-    staking_test.checkValidatorSet(4, True, 3)
+    staking_test.stake('6', addr_list[1], True)
+    staking_test.checkStatus(addr_list[1], 6, 0, False)
+    staking_test.checkValidatorSet(4, True, 6)
     time.sleep(5)
 
     staking_test.unstake('1', addr_list[1], True)
-    staking_test.checkStatus(addr_list[1], 2, 0, True)
-    staking_test.checkValidatorSet(4, True, 2)
+    staking_test.checkStatus(addr_list[1], 5, 0, True)
+    staking_test.checkValidatorSet(4, True, 5)
     time.sleep(5)
 
     staking_test.stake('1', addr_list[2], False)
     staking_test.unstake('1', addr_list[2], False)
 
-    staking_test.unstake('2', addr_list[1], True)
+    staking_test.unstake('5', addr_list[1], True)
     staking_test.checkStatus(addr_list[1], 0, 0, True)
     staking_test.checkValidatorSet(4, False, 0)
 
@@ -31,7 +31,7 @@ if __name__ == "__main__":
         print(str(_wait - x) + "s left")
         time.sleep(1)
 
-    staking_test.checkStatus(addr_list[1], 0, 3, False)
-    staking_test.withdraw('3', addr_list[1])
+    staking_test.checkStatus(addr_list[1], 0, 6, False)
+    staking_test.withdraw('6', addr_list[1])
     staking_test.checkStatus(addr_list[1], 0, 0, False)
 
