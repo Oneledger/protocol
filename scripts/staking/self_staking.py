e3b88923a821b492d32df6105b76fc3c4b595e0c
Tanmay <tanmay@oneledger.io>
2020-07-30 12:35:10 -0400

    changing staking test

--- scripts/staking/self_staking.py
+++ scripts/staking/self_staking.py
@@ -1,42 +1,45 @@
-from time import sleep
-
 from sdk import *
 
 _wait = 50
 
 if __name__ == "__main__":
     staking_test = Staking(node_4)
-    staking_test.prepare()
+    new_staking_address = staking_test.prepare()
     addr_list = addresses()
+    original_staking_address = staking_test.staking_address
 
     # stake
-    staking_test.stake('6', addr_list[1], True)
-    staking_test.checkStatus(addr_list[1], 6, 0, False)
-    staking_test.checkValidatorSet(4, True, 6)
+    staking_test.stake('4000000', True)
+    staking_test.checkStatus(4000000, 0, False)
+    staking_test.checkValidatorSet(4, True, 4000000)
     time.sleep(2)
 
     # unstake
-    staking_test.unstake('1', addr_list[1], True)
-    staking_test.checkStatus(addr_list[1], 5, 0, True)
-    staking_test.checkValidatorSet(4, True, 5)
+    staking_test.unstake('1000000', True)
+    staking_test.checkStatus(3000000, 0, True)
+    staking_test.checkValidatorSet(4, True, 3000000)
     time.sleep(2)
 
     # stake from different address, should fail due to stake address mismatch
-    staking_test.stake('1', addr_list[2], False)
+
+    staking_test.staking_address = new_staking_address
+    staking_test.stake('1', False)
 
     # unstake from different address, should fail due to stake address mismatch
-    staking_test.unstake('1', addr_list[2], False)
+    staking_test.unstake('1', False)
 
     # unstake all
-    staking_test.unstake('5', addr_list[1], True)
-    staking_test.checkStatus(addr_list[1], 0, 0, True)
+    staking_test.staking_address = original_staking_address
+    staking_test.unstake('3000000', True)
+    staking_test.checkStatus(0, 0, True)
     staking_test.checkValidatorSet(4, False, 0)
 
     # stake again using new stake address.
     # We allow this Tx, but you won't be able to withdraw old money due to stake address mismatch
-    staking_test.stake('6', addr_list[2], True)
-    staking_test.checkStatus(addr_list[2], 6, 0, False)
-    staking_test.checkValidatorSet(4, True, 6)
+    staking_test.staking_address = new_staking_address
+    staking_test.stake('4000000', True)
+    staking_test.checkStatus(4000000, 0, False)
+    staking_test.checkValidatorSet(4, True, 4000000)
 
     # waits for above unstaked old amount to mature
     print("wait for " + str(_wait) + "s")
@@ -45,16 +48,18 @@ if __name__ == "__main__":
         time.sleep(1)
 
     # withdraw old money, should fail due to stake address mismatch
-    staking_test.checkStatus(addr_list[1], 0, 6, False)
-    staking_test.withdraw('6', addr_list[1], False)
+    staking_test.staking_address = original_staking_address
+    staking_test.checkStatus(0, 4000000, False)
+    staking_test.withdraw('6', False)
 
     # unstake all new money
-    staking_test.unstake('6', addr_list[2], True)
-    staking_test.checkStatus(addr_list[2], 0, 0, True)
+    staking_test.staking_address = new_staking_address
+    staking_test.unstake('4000000', True)
+    staking_test.checkStatus(0, 0, True)
     staking_test.checkValidatorSet(4, False, 0)
 
     # withdraw old money again, should success
-    staking_test.checkStatus(addr_list[1], 0, 6, False)
-    staking_test.withdraw('6', addr_list[1], True)
-    staking_test.checkStatus(addr_list[1], 0, 0, False)
-
+    staking_test.staking_address = original_staking_address
+    staking_test.checkStatus(0, 4000000, False)
+    staking_test.withdraw('4000000', True)
+    staking_test.checkStatus(0, 0, False)
