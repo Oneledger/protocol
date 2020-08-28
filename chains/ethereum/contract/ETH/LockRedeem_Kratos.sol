e84018da1ab9e41df79e8e3131a540a69edee2e2
Tanmay Bhattacharya <tanmay.bhattacharya.smit@gmail.com>
2020-08-28 12:15:40 -0400

    Upgrade smartcontract abi (#638)
    Added the upgraded abi for new smart contract

--- chains/ethereum/contract/ETH/LockRedeem_Kratos.sol
+++ chains/ethereum/contract/ETH/LockRedeem_Kratos.sol
@@ -7,7 +7,7 @@ contract LockRedeem {
     // numValidators holds the total number of validators
     uint public numValidators;
 
-    mapping (address => uint8) public validators;
+    mapping(address => uint8) public validators;
     address[] validatorList;
 
     // Require this amount of signatures to push a proposal through,Set only in constructor
@@ -18,15 +18,15 @@ contract LockRedeem {
     //the cost of a contract deployment (32000 gas) [Not relevant for sign trasaction]
     //the cost for every zero byte of data or code for a transaction.
     //the cost of every non-zero byte of data or code for a transaction.
-    uint transactionCost = 23192 ;
-    uint additionalGasCost = 37692 ;
+    uint transactionCost = 23192;
+    uint additionalGasCost = 37692;
     uint redeemGasCharge = 10000000000000000;
     uint validatorEarningMultiplier = 1;
     uint rewardGas = 10000;
 
     uint public migrationSignatures;
-    mapping (address => bool) public migrationSigners;
-    mapping (address => uint) migrationCount;
+    mapping(address => bool) public migrationSigners;
+    mapping(address => uint) migrationCount;
     address [] migrationAddress;
 
     // Default Voting power should be updated at one point
@@ -39,6 +39,10 @@ contract LockRedeem {
     // 28800 old value
     uint256 LOCK_PERIOD;
 
+    // Variables for migrate
+    address old_contract;
+    uint signaturesrequiredformigration = 0;
+    uint migrationsignaturecount = 0;
 
     struct RedeemTX {
         address payable recipient;
@@ -48,7 +52,8 @@ contract LockRedeem {
         uint256 until;
         uint256 redeemFee;
     }
-    mapping (address => RedeemTX) redeemRequests;
+
+    mapping(address => RedeemTX) redeemRequests;
 
     event RedeemRequest(
         address indexed recepient,
@@ -88,13 +93,14 @@ contract LockRedeem {
     }
 
     modifier onlyValidator() {
-        require(isValidator(msg.sender) ,"validator not present in List");
-        _; // Continues control flow after this is validates
+        require(isValidator(msg.sender), "validator not present in List");
+        _;
+        // Continues control flow after this is validates
     }
 
 
 
-    constructor(address[] memory initialValidators,uint _lock_period) public {
+    constructor(address[] memory initialValidators, uint _lock_period, address _old_contract, uint noofValidatorsinold) public {
         // Require at least 4 validators
         require(initialValidators.length >= MIN_VALIDATORS, "insufficient validators passed to constructor");
 
@@ -105,21 +111,22 @@ contract LockRedeem {
             require(validators[v] == 0, "found non-unique validator in initialValidators");
             addValidator(v);
         }
-        ACTIVE = true ;
         LOCK_PERIOD = _lock_period;
         //validatorEarningMultiplier = multiplier;
         votingThreshold = (initialValidators.length * 2 / 3) + 1;
         activeThreshold = (initialValidators.length * 1 / 3) + 1;
+        old_contract = _old_contract;
+        signaturesrequiredformigration = (noofValidatorsinold * 2 / 3 + 1);
 
     }
 
-    function migrate (address newSmartContractAddress) public onlyValidator {
-        require(migrationSigners[msg.sender]==false,"Validator Signed already");
-        migrationSigners[msg.sender] = true ;
+    function migrate(address newSmartContractAddress) public onlyValidator {
+        require(migrationSigners[msg.sender] == false, "Validator Signed already");
+        migrationSigners[msg.sender] = true;
         (bool status,) = newSmartContractAddress.call(abi.encodePacked(bytes4(keccak256("MigrateFromOld()"))));
-        require(status,"Unable to Migrate new Smart contract");
+        require(status, "Unable to Migrate new Smart contract");
         migrationSignatures = migrationSignatures + 1;
-        if(migrationCount[newSmartContractAddress]==0)
+        if (migrationCount[newSmartContractAddress] == 0)
         {
             migrationAddress.push(newSmartContractAddress);
         }
@@ -127,58 +134,58 @@ contract LockRedeem {
 
         // Global flag ,needs to be set only once
         if (migrationSignatures == activeThreshold) {
-            ACTIVE = false ;
+            ACTIVE = false;
         }
         // Transfer needs to be done only once
         if (migrationSignatures == votingThreshold) {
-            uint voteCount = 0 ;
-            address maxVotedAddress ;
-            for(uint i=0;i<migrationAddress.length;i++){
-                if (migrationCount[migrationAddress[i]] > voteCount){
+            uint voteCount = 0;
+            address maxVotedAddress;
+            for (uint i = 0; i < migrationAddress.length; i++) {
+                if (migrationCount[migrationAddress[i]] > voteCount) {
                     voteCount = migrationCount[migrationAddress[i]];
-                    maxVotedAddress =migrationAddress[i];
+                    maxVotedAddress = migrationAddress[i];
                 }
             }
-            (bool success, ) = maxVotedAddress.call.value(address(this).balance)("");
+            (bool success,) = maxVotedAddress.call.value(address(this).balance)("");
             require(success, "Transfer failed");
         }
     }
     // function called by user
     function lock() payable public isActive {
         require(msg.value >= 0, "Must pay a balance more than 0");
-        emit Lock(msg.sender,msg.value);
+        emit Lock(msg.sender, msg.value);
     }
 
     // function called by user
-    function redeem(uint256 amount_)  payable public isActive {
-        require(isredeemAvailable(msg.sender) ,"redeem to this address is not available yet");
+    function redeem(uint256 amount_) payable public isActive {
+        require(isredeemAvailable(msg.sender), "redeem to this address is not available yet");
         require(amount_ > 0, "amount should be bigger than 0");
-        require(msg.value + redeemRequests[msg.sender].redeemFee >= redeemGasCharge,"Redeem fee not provided");
+        require(msg.value + redeemRequests[msg.sender].redeemFee >= redeemGasCharge, "Redeem fee not provided");
 
         redeemRequests[msg.sender].signature_count = uint256(0);
         redeemRequests[msg.sender].recipient = msg.sender;
-        redeemRequests[msg.sender].amount = amount_ ;
+        redeemRequests[msg.sender].amount = amount_;
         redeemRequests[msg.sender].until = block.number + LOCK_PERIOD;
         redeemRequests[msg.sender].redeemFee += msg.value;
         redeemRequests[msg.sender].votes = 0;
-        emit RedeemRequest(redeemRequests[msg.sender].recipient,redeemRequests[msg.sender].amount,redeemRequests[msg.sender].redeemFee);
+        emit RedeemRequest(redeemRequests[msg.sender].recipient, redeemRequests[msg.sender].amount, redeemRequests[msg.sender].redeemFee);
     }
 
     //function called by protocol
     function sign(uint amount_, address payable recipient_) public isActive onlyValidator {
         uint startGas = gasleft();
-        require(isValidator(msg.sender),"validator not present in list");
+        require(isValidator(msg.sender), "validator not present in list");
         require(redeemRequests[recipient_].until > block.number, "redeem request is not available");
-        require(redeemRequests[recipient_].amount == amount_,"redeem amount is different" );
-        require((redeemRequests[recipient_].votes >> validators[msg.sender])% 2 == uint256(0), "validator has already voted");
+        require(redeemRequests[recipient_].amount == amount_, "redeem amount is different");
+        require((redeemRequests[recipient_].votes >> validators[msg.sender]) % 2 == uint256(0), "validator has already voted");
 
         // update votes
         redeemRequests[recipient_].votes = redeemRequests[recipient_].votes + (uint256(1) << validators[msg.sender]);
         redeemRequests[recipient_].signature_count += 1;
 
         // if threshold is reached, transfer
-        if (redeemRequests[recipient_].signature_count >= votingThreshold ) {
-            (bool success, ) = redeemRequests[recipient_].recipient.call.value((redeemRequests[recipient_].amount))("");
+        if (redeemRequests[recipient_].signature_count >= votingThreshold) {
+            (bool success,) = redeemRequests[recipient_].recipient.call.value((redeemRequests[recipient_].amount))("");
             require(success, "Transfer failed.");
             redeemRequests[recipient_].amount = 0;
             redeemRequests[recipient_].until = block.number;
@@ -188,54 +195,54 @@ contract LockRedeem {
         // additionalGasCost is the extra gas used by the lines of code after gas calculation is done.
         // This is an approximate calcualtion and actuall cost might vary sightly .
 
-        uint gasUsed = startGas - gasleft() + transactionCost + additionalGasCost ;
+        uint gasUsed = startGas - gasleft() + transactionCost + additionalGasCost;
         uint gasFee = gasUsed * tx.gasprice + (rewardGas * validatorEarningMultiplier);
-        (bool success, ) = msg.sender.call.value(gasFee)("");
+        (bool success,) = msg.sender.call.value(gasFee)("");
         require(success, "Transfer back to validator failed");
         redeemRequests[recipient_].redeemFee -= gasFee;
-        emit ValidatorSignedRedeem(recipient_, msg.sender, amount_,gasFee);
+        emit ValidatorSignedRedeem(recipient_, msg.sender, amount_, gasFee);
     }
 
 
     function collectUserFee() public isActive {
-        require(verifyRedeem(msg.sender) > 0 , "request signing is still in progress");
-        (bool success, ) = msg.sender.call.value(redeemRequests[msg.sender].redeemFee)("");
+        require(verifyRedeem(msg.sender) > 0, "request signing is still in progress");
+        (bool success,) = msg.sender.call.value(redeemRequests[msg.sender].redeemFee)("");
         require(success, "Transfer failed.");
     }
 
-    function isValidator(address addr) public view returns(bool) {
+    function isValidator(address addr) public view returns (bool) {
         return validators[addr] > 0;
     }
 
-    function isredeemAvailable (address recepient_) public isActive view returns (bool)  {
+    function isredeemAvailable(address recepient_) public isActive view returns (bool)  {
         return redeemRequests[recepient_].until < block.number;
     }
 
-    function hasValidatorSigned(address recipient_) public isActive view returns(bool) {
-        return  ((redeemRequests[recipient_].votes >> validators[msg.sender]) % 2 == uint256(1));
+    function hasValidatorSigned(address recipient_) public isActive view returns (bool) {
+        return ((redeemRequests[recipient_].votes >> validators[msg.sender]) % 2 == uint256(1));
     }
 
     //BeforeRedeem : -1  (amount == 0 and until = 0)
     //Ongoing : 0  (amount > 0 and until > block.number)
     //Success : 1  (amount = 0 and until < block.number)
     //Expired : 2  (amount > 0 and until < block.number)
-    function verifyRedeem(address recipient_) public isActive view returns(int8){
-        return (redeemRequests[recipient_].until > 0 && redeemRequests[recipient_].until < block.number) ? (redeemRequests[recipient_].amount > 0? int8(2) : int8(1) ): (redeemRequests[recipient_].amount == 0 ? int8(-1) : int8(0));
+    function verifyRedeem(address recipient_) public isActive view returns (int8){
+        return (redeemRequests[recipient_].until > 0 && redeemRequests[recipient_].until < block.number) ? (redeemRequests[recipient_].amount > 0 ? int8(2) : int8(1)) : (redeemRequests[recipient_].amount == 0 ? int8(- 1) : int8(0));
     }
 
-    function getSignatureCount(address recipient_) public isActive view returns(uint256){
+    function getSignatureCount(address recipient_) public isActive view returns (uint256){
         return redeemRequests[recipient_].signature_count;
     }
 
-    function getRedeemBalance(address recipient_) public isActive view returns(uint256){
+    function getRedeemBalance(address recipient_) public isActive view returns (uint256){
         return redeemRequests[recipient_].redeemFee;
     }
 
-    function getTotalEthBalance() public view returns(uint) {
-        return address(this).balance ;
+    function getTotalEthBalance() public view returns (uint) {
+        return address(this).balance;
     }
 
-    function getOLTEthAddress() public view returns(address){
+    function getOLTEthAddress() public view returns (address){
         return address(this);
     }
 
@@ -244,6 +251,21 @@ contract LockRedeem {
         validatorList.push(v);
         validators[v] = uint8(validatorList.length);
         emit AddValidator(v);
+    }
+
+    function MigrateFromOld() public {
+        require(msg.sender == old_contract);
+        migrationsignaturecount = migrationsignaturecount + 1;
+        //addValidator(tx.origin);
+    }
+
+    function() external payable {
+        require(migrationsignaturecount == signaturesrequiredformigration);
+        ACTIVE = true;
+    }
+
+    function ActiveStatus() public view returns (bool) {
+        return ACTIVE;
     }
 
 }
