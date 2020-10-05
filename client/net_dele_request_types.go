73fc788e28cfc2c56162b8007590795637f142ab
Hao <haosun@oneledger.io>
2020-10-05 17:06:35 -0400

    add online serialization and service

--- client/net_dele_request_types.go
+++ client/net_dele_request_types.go
@@ -2,13 +2,19 @@ package client
 
 import (
 	"github.com/Oneledger/protocol/action"
+	"github.com/Oneledger/protocol/data/balance"
 	"github.com/Oneledger/protocol/data/keys"
 )
 
-//-------------------TX-----------------
+//-------------------Query
+type GetTotalNetwkDelgReply struct {
+	Amount balance.Amount `json:"amount"`
+}
+
+//-------------------Tx-----------------
 type FinalizeRewardsRequest struct {
-	Delegator keys.Address   `json:"delegator"`
+	Delegator keys.Address  `json:"delegator"`
 	Amount    action.Amount `json:"amount"`
-	GasPrice  action.Amount  `json:"gasPrice"`
-	Gas       int64          `json:"gas"`
+	GasPrice  action.Amount `json:"gasPrice"`
+	Gas       int64         `json:"gas"`
 }
---
+++ service/query/network_delegation.go
@@ -0,0 +1,15 @@
+package query
+
+import (
+	"github.com/Oneledger/protocol/client"
+)
+
+func (svc *Service) GetTotalNetwkDelg(reply *client.GetTotalNetwkDelgReply) error {
+	// TODO get pool balance
+
+	//*reply = client.GetTotalNetwkDelgReply{
+	//	Amount:
+	//}
+
+	return nil
+}
