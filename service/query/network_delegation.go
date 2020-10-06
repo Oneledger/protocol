793d17c4883bd6ca162198aad12577fde391d1aa
Hao <haosun@oneledger.io>
2020-10-06 12:44:02 -0400

    finish query OLP-1169

--- service/query/network_delegation.go
+++ service/query/network_delegation.go
@@ -1,15 +1,26 @@
 package query
 
 import (
+	"errors"
 	"github.com/Oneledger/protocol/client"
 )
 
 func (svc *Service) GetTotalNetwkDelg(reply *client.GetTotalNetwkDelgReply) error {
-	// TODO get pool balance
+	poolList, err := svc.governance.GetPoolList()
+	if err != nil {
+		return err
+	}
+	if _, ok := poolList["DelegationPool"]; !ok {
+		return errors.New("failed to get network delegation pool")
+	}
+	delagationPool := poolList["DelegationPool"]
 
-	//*reply = client.GetTotalNetwkDelgReply{
-	//	Amount:
-	//}
-
+	balance, err := svc.balances.GetBalance(delagationPool, svc.currencies)
+	if err != nil {
+		return err
+	}
+	*reply = client.GetTotalNetwkDelgReply{
+		Amount: *balance.Amounts["OLT"].Amount,
+	}
 	return nil
 }
