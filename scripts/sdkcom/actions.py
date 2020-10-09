e24d4b6266ec39d4367c74328fa46d7dc2e26c3c
charlie <charlie@oneledger.io>
2020-10-09 15:51:44 -0400

    initiated common sdk

--- scripts/sdkcom/actions.py
+++ scripts/sdkcom/actions.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 from rpc_call import *
 
@@ -39,3 +40,25 @@ def broadcast_sync(raw_tx, signature, pub_key):
         "publicKey": pub_key,
     })
     return resp["result"]
+
+def query_balance(address):
+    req = {
+        "currency": "OLT",
+        "address": address
+    }
+    resp = rpc_call('query.CurrencyBalance', req)
+
+    if "result" in resp:
+        result = resp["result"]["balance"]
+    else:
+        result = ""
+    return int(float(result))
+
+def wait_for(blocks, url=url_0):
+    resp = rpc_call('query.ListValidators', {}, url)
+    hstart = resp["result"]["height"]
+    hcur = hstart
+    while hcur - hstart < blocks:
+        time.sleep(0.5)
+        resp = rpc_call('query.ListValidators', {}, url)
+        hcur = resp["result"]["height"]
