8a0216185be577757fe9d5429f56ce3f6a781fb7
kingpinXD <tanmay@oneledger.io>
2020-11-10 18:03:52 -0500

    BT1

--- mem_collector.sh
+++ mem_collector.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-ps -o pid,user,%mem,command ax | sort -b -k3 -r | grep olfullnode >>/opt/data/devnet/ST_NOEVI_NOREWARDS/ram_usage.log
-pmap 14797 | tail -n 1 >>/opt/data/devnet/ST_NOEVI_NOREWARDS/ram_usage.log
-curl -sK -v http://localhost:45131/debug/pprof/heap >/opt/data/devnet/ST_NOEVI_NOREWARDS/$(date +\%Y\%m\%d\%H\%M\%S)-heap.out
-echo $(date +\%Y\%m\%d\%H\%M\%S) >>/opt/data/devnet/ST_NOEVI_NOREWARDS/ram_usage.log
-echo "-------------------------------------------------------------------------------------------------" >>/opt/data/devnet/ST_NOEVI_NOREWARDS/ram_usage.log
+ps -o pid,user,%mem,command ax | sort -b -k3 -r | grep olfullnode >>/opt/ST_FULL_APP/ram_usage.log
+curl -sK -v http://localhost:42637/debug/pprof/heap >/opt/ST_FULL_APP/$(date +\%Y\%m\%d\%H\%M\%S)-heap.out
+echo $(date +\%Y\%m\%d\%H\%M\%S) >>/opt/ST_FULL_APP/ram_usage.log
+echo "-------------------------------------------------------------------------------------------------" >>/opt/ST_FULL_APP/ram_usage.log
