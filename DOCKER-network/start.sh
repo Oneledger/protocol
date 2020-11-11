bac5bcc8e9553f59d27c665643e8f535bbb6c7d9
KevinOneLedger <kevin@oneledger.io>
2020-11-11 11:05:58 -0500

    OLP-1269: Docker compose -updated scripts and Dockerfile -changed name of image in docker-compose.yml

--- DOCKER-network/start.sh
+++ DOCKER-network/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [ ! -d "$OLDATA/docker" ]
+then
+    mkdir "$OLDATA"/docker
+fi
+
 sudo rm -rf $OLDATA/docker/*-Node
+sudo rm -rf $OLDATA/docker/bin/*
 
 #start build container
 docker-compose -f docker-compose-build.yml up -d
@@ -8,7 +14,7 @@ docker-compose -f docker-compose-build.yml up -d
 #wait until build container creates config files
 until [ -n "$(find ~/oldata/docker -name '*Node' -type d)" ]; do
   echo "building..."
-  sleep 3
+  sleep 5
 done
 
 #start node containers
--- DOCKER-network/update.sh
+++ DOCKER-network/update.sh
@@ -7,7 +7,6 @@ docker container kill $(docker ps -q)
 #Build Binaries
 docker start builder
 docker exec -it builder bash -c "cd /home/ubuntu/go/protocol && make install_c"
-docker stop builder
 
 #Start nodes
 docker-compose up -d
