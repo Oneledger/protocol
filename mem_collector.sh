25783211bbaa5d77bf976d4eccbaac1d881a42ea
kingpinXD <tanmay@oneledger.io>
2020-11-18 14:55:06 -0500

    meme_commector

--- mem_collector.sh
+++ mem_collector.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ps -o pid,user,%mem,vsz,rss,command ax | sort -b -k3 -r | grep olfullnode >>/opt/ST_COMM/ram_usage.log
-curl -sK -v http://localhost:33077/debug/pprof/heap >/opt/ST_COMM/$(date +\%Y\%m\%d\%H\%M\%S)-heap.out
-echo $(date +\%Y\%m\%d\%H\%M\%S) >>/opt/ST_COMM/ram_usage.log
-echo "-------------------------------------------------------------------------------------------------" >>/opt/ST_COMM/ram_usage.log
+ps -o pid,user,%mem,vsz,rss,command ax | sort -b -k3 -r | grep olfullnode >>/opt/ST_MFULLNEWOPT/ram_usage.log
+curl -sK -v http://localhost:33077/debug/pprof/heap >/opt/ST_MFULLNEWOPT/$(date +\%Y\%m\%d\%H\%M\%S)-heap.out
+echo $(date +\%Y\%m\%d\%H\%M\%S) >>/opt/ST_MFULLNEWOPT/ram_usage.log
+echo "-------------------------------------------------------------------------------------------------" >>/opt/ST_MFULLNEWOPT/ram_usage.log
