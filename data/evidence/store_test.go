18116efd0c09c4c99c785d59c672cea9804e72a9
charlie <charlie@oneledger.io>
2020-12-08 17:35:39 -0500

    unit test for evidence and allegation genesis dump

--- data/evidence/store_test.go
+++ data/evidence/store_test.go
@@ -6,77 +6,110 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	db "github.com/tendermint/tm-db"
 
-	"github.com/Oneledger/protocol/data/balance"
 	"github.com/Oneledger/protocol/data/keys"
 	"github.com/Oneledger/protocol/storage"
 )
 
 var (
-	memDb      db.DB
-	store      *EvidenceStore
-	cs         *storage.State
-	validator1 keys.Address
-	validator2 keys.Address
-	validator3 keys.Address
-	zero       *balance.Amount
-	amt1       *balance.Amount
-	amt2       *balance.Amount
-	amt3       *balance.Amount
-	amt4       *balance.Amount
-	amt5       *balance.Amount
+	memDb       db.DB
+	store       *EvidenceStore
+	cs          *storage.State
+	validators  []keys.Address
+	allegations []*AllegationRequest
+	suspicious  []*LastValidatorHistory
 )
 
 func setupStore() {
 	fmt.Println("####### Testing  evidence store #######")
 	memDb = db.NewDB("test", db.MemDBBackend, "")
-	cs = storage.NewState(storage.NewChainState("rewards", memDb))
+	cs = storage.NewState(storage.NewChainState("state", memDb))
 
 	store = NewEvidenceStore("es", cs)
-	generateAddresses()
+	generateEvidences()
 }
 
-func generateAddresses() {
-	pub1, _, _ := keys.NewKeyPairFromTendermint()
-	h1, _ := pub1.GetHandler()
-	validator1 = h1.Address()
+func generateEvidences() {
+	for i := 0; i < 3; i++ {
+		pub, _, _ := keys.NewKeyPairFromTendermint()
+		h, _ := pub.GetHandler()
+		validators = append(validators, h.Address())
+	}
+	sort.Slice(validators, func(i, j int) bool {
+		return validators[i].String() < validators[j].String()
+	})
 
-	pub2, _, _ := keys.NewKeyPairFromTendermint()
-	h2, _ := pub2.GetHandler()
-	validator2 = h2.Address()
+	// create allegations
+	id1, _ := store.GenerateRequestID()
+	id2, _ := store.GenerateRequestID()
+	ar1 := NewAllegationRequest(id1, validators[0], validators[1], 10, "did something bad")
+	ar2 := NewAllegationRequest(id2, validators[0], validators[2], 20, "too many votes")
 
-	pub3, _, _ := keys.NewKeyPairFromTendermint()
-	h3, _ := pub2.GetHandler()
-	validator3 = h3.Address()
-
-	zero = balance.NewAmount(0)
-	amt1 = balance.NewAmount(100)
-	amt2 = balance.NewAmount(200)
-	amt3 = balance.NewAmount(377)
+	// sort allegations
+	allegations = []*AllegationRequest{ar1, ar2}
+	sort.Slice(allegations, func(i, j int) bool {
+		return allegations[i].ID < allegations[j].ID
+	})
 }
 
 func TestRewardsCumulativeStore_DumpLoadState(t *testing.T) {
 	setupStore()
 
-	// create allegations
-	id1, _ := store.GenerateRequestID()
-	id2, _ := store.GenerateRequestID()
-	ar1 := NewAllegationRequest(id1, validator1, validator2, 10, "did something bad")
-	ar2 := NewAllegationRequest(id2, validator1, validator3, 20, "too many votes")
-	err := store.SetAllegationRequest(ar1)
+	// create allegation1 and vote
+	err := store.SetAllegationRequest(allegations[0])
 	assert.Nil(t, err)
-	err = store.SetAllegationRequest(ar2)
+	vote1 := &AllegationVote{
+		Address: validators[0],
+		Choice:  YES,
+	}
+	vote2 := &AllegationVote{
+		Address: validators[1],
+		Choice:  NO,
+	}
+	allegations[0].Votes = []*AllegationVote{vote1, vote2}
+	err = store.Vote(allegations[0].ID, vote1.Address, vote1.Choice)
 	assert.Nil(t, err)
-	err = store.SetAllegationRequest(ar1)
-	store.state.Commit()
+	err = store.Vote(allegations[0].ID, vote2.Address, vote2.Choice)
+	assert.Nil(t, err)
+
+	// create allegation2 and vote
+	err = store.SetAllegationRequest(allegations[1])
+	assert.Nil(t, err)
+	vote3 := &AllegationVote{
+		Address: validators[0],
+		Choice:  YES,
+	}
+	vote4 := &AllegationVote{
+		Address: validators[1],
+		Choice:  YES,
+	}
+	allegations[1].Votes = []*AllegationVote{vote3, vote4}
+	err = store.Vote(allegations[1].ID, vote3.Address, vote3.Choice)
+	assert.Nil(t, err)
+	err = store.Vote(allegations[1].ID, vote4.Address, vote4.Choice)
+	assert.Nil(t, err)
+
+	// create suspicious validators
+	now := time.Now()
+	tm1 := now.AddDate(0, 0, 1)
+	tm2 := tm1.AddDate(0, 0, 7)
+	lvh1, _ := store.CreateSuspiciousValidator(validators[1], MISSED_REQUIRED_VOTES, 1100, &tm2)
+	lvh2, _ := store.CreateSuspiciousValidator(validators[2], BYZANTINE_FAULT, 5600, &tm2)
+	assert.Nil(t, err)
+	suspicious = []*LastValidatorHistory{lvh1, lvh2}
 
 	// dump chain state
+	store.state.Commit()
 	state, succeed := store.DumpState()
 	assert.True(t, succeed)
+	assert.EqualValues(t, allegations, state.Allegations)
+	assert.EqualValues(t, suspicious, state.SuspiciousValidators)
 
 	// create Genesis file
 	dir, _ := os.Getwd()
@@ -100,5 +133,5 @@ func TestRewardsCumulativeStore_DumpLoadState(t *testing.T) {
 	stateLoaded := NewEvidenceState()
 	err = json.Unmarshal(stateBytes, stateLoaded)
 	assert.Nil(t, err)
-	assert.Equal(t, state, stateLoaded)
+	assert.EqualValues(t, state, stateLoaded)
 }
