580ae7f76eccb2d961901aa9e632f017c2f2adfb
DeV1doR <bogdankurinniy.dev1@gmail.com>
2021-07-06 20:26:41 +0300

    Added new eth functionality

--- web3/eth/misc.go
+++ web3/eth/misc.go
@@ -1,12 +1,81 @@
 package eth
 
 import (
+	"math/big"
+
 	"github.com/Oneledger/protocol/utils"
+	"github.com/Oneledger/protocol/version"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
+// ProtocolVersion returns the supported Ethereum protocol version.
+func (svc *Service) ProtocolVersion() string {
+	svc.logger.Debug("eth_protocolVersion")
+	return version.Protocol.String()
+}
+
+// ChainId returns the chain's identifier in hex format
 func (svc *Service) ChainId() hexutil.Big {
-	block := svc.ext.Block(0).Block
+	svc.logger.Debug("eth_chainId")
+	block := svc.ctx.GetAPI().Block(0).Block
 	chainID := utils.HashToBigInt(block.Header.ChainID)
 	return hexutil.Big(*chainID)
+}
+
+// Syncing returns whether or not the current node is syncing with other peers. Returns false if not, or a struct
+// outlining the state of the sync if it is.
+func (svc *Service) Syncing() (interface{}, error) {
+	svc.logger.Debug("eth_syncing")
+
+	status, err := svc.getTMClient().Status()
+	if err != nil {
+		return false, err
+	}
+
+	if !status.SyncInfo.CatchingUp {
+		return false, nil
+	}
+
+	return map[string]interface{}{
+		// "startingBlock": nil, // NA
+		"currentBlock": hexutil.Uint64(status.SyncInfo.LatestBlockHeight),
+		// "highestBlock":  nil, // NA
+		// "pulledStates":  nil, // NA
+		// "knownStates":   nil, // NA
+	}, nil
+}
+
+// Coinbase is the address that staking rewards will be send to (alias for Etherbase).
+func (svc *Service) Coinbase() (common.Address, error) {
+	svc.logger.Debug("eth_coinbase")
+
+	pubKeyHander, err := svc.ctx.GetNodeContext().PubKey().GetHandler()
+	if err != nil {
+		return common.Address{}, err
+	}
+
+	return common.BytesToAddress(pubKeyHander.Bytes()), nil
+}
+
+// Mining returns whether or not this node is currently mining. Always false.
+func (svc *Service) Mining() bool {
+	svc.logger.Debug("eth_mining")
+	// TODO: Add info in according if this node is validator and it is active
+	return false
+}
+
+// Hashrate returns the current node's hashrate. Always 0.
+func (svc *Service) Hashrate() hexutil.Uint64 {
+	svc.logger.Debug("eth_hashrate")
+	return 0
+}
+
+// GasPrice returns the current gas price
+func (svc *Service) GasPrice() *hexutil.Big {
+	svc.logger.Debug("eth_gasPrice")
+	// NOTE: Maybe we could add the mean price in according to previous blocks
+	// but not really need it right now
+	out := big.NewInt(0)
+	return (*hexutil.Big)(out)
 }
--- web3/eth/service.go
+++ web3/eth/service.go
@@ -4,37 +4,32 @@ import (
 	"errors"
 	"sync"
 
-	"github.com/Oneledger/protocol/action"
-	"github.com/Oneledger/protocol/client"
 	"github.com/Oneledger/protocol/log"
+
 	"github.com/Oneledger/protocol/storage"
+	web3types "github.com/Oneledger/protocol/web3/types"
 	"github.com/ethereum/go-ethereum/rpc"
 
 	rpcclient "github.com/Oneledger/protocol/client"
 )
 
 type Service struct {
-	log     *log.Logger
-	ext     client.ExtServiceContext
-	stateDB *action.CommitStateDB
+	ctx    web3types.Web3Context
+	logger *log.Logger
 
 	mu sync.Mutex
 }
 
-func NewService(logger *log.Logger, ext client.ExtServiceContext, stateDB *action.CommitStateDB) *Service {
-	return &Service{
-		log:     logger,
-		ext:     ext,
-		stateDB: stateDB,
-	}
+func NewService(ctx web3types.Web3Context) *Service {
+	return &Service{ctx: ctx, logger: ctx.GetLogger()}
 }
 
 func (svc *Service) getTMClient() rpcclient.Client {
-	return svc.ext.RPCClient()
+	return svc.ctx.GetAPI().RPCClient()
 }
 
 func (svc *Service) getState() *storage.State {
-	return svc.stateDB.GetContractStore().State
+	return svc.ctx.GetContractStore().State
 }
 
 func (svc *Service) getStateHeight(height int64) int64 {
