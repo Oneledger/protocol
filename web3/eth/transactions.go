65a048896d2265e01c7d2fe568ff3a0092e474b5
DeV1doR <bogdankurinniy.dev1@gmail.com>
2021-07-08 14:34:43 +0300

    Add more methods for getting transactions

--- web3/eth/transactions.go
+++ web3/eth/transactions.go
@@ -3,12 +3,15 @@ package eth
 import (
 	"math/big"
 
+	"github.com/Oneledger/protocol/action"
+	"github.com/Oneledger/protocol/utils"
 	rpctypes "github.com/Oneledger/protocol/web3/types"
 	rpcutils "github.com/Oneledger/protocol/web3/utils"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/rpc"
+	tmtypes "github.com/tendermint/tendermint/types"
 )
 
 // GetTransactionCount returns the number of transactions at the given address up to the given block number.
@@ -30,7 +33,7 @@ func (svc *Service) GetTransactionCount(address common.Address, blockNrOrHash rp
 	txLen := ethAcc.Sequence
 
 	// for pending
-	if height == -2 {
+	if height == rpctypes.PendingBlockNumber {
 		txLen = ethAcc.Sequence + rpcutils.GetPendingTxCountByAddress(svc.getTMClient(), address)
 	}
 	n := hexutil.Uint64(txLen)
@@ -71,7 +74,7 @@ func (svc *Service) GetTransactionByHash(hash common.Hash) (*rpctypes.Transactio
 }
 
 // GetTransactionReceipt returns the transaction receipt identified by hash.
-func (svc *Service) GetTransactionReceipt(hash common.Hash) (map[string]interface{}, error) {
+func (svc *Service) GetTransactionReceipt(hash common.Hash) (*rpctypes.TransactionReceipt, error) {
 	svc.mu.Lock()
 	defer svc.mu.Unlock()
 
@@ -105,41 +108,119 @@ func (svc *Service) GetTransactionReceipt(hash common.Hash) (map[string]interfac
 	}
 
 	// Set status codes based on tx result
-	status := hexutil.Uint(resTx.TxResult.Code)
+	status := hexutil.Uint64(resTx.TxResult.Code)
 
-	// TODO: Implement this
-	logs := []*ethtypes.Log{}
+	stateDB := action.NewCommitStateDB(svc.ctx.GetContractStore(), svc.ctx.GetAccountKeeper(), svc.logger)
+
+	logs, err := stateDB.GetLogs(hash)
+	if err != nil {
+		return nil, err
+	}
+	// TODO: Implement bloom
 	bloom := ethtypes.BytesToBloom(make([]byte, 6))
 
-	// TODO: Add handle if tx type is smart contract
-	contractAddress := common.Address{}
+	var contractAddress *common.Address
 	if tx.To == nil {
-		contractAddress = common.Address{}
+		contractAddress = rpctypes.GetContractAddress(&resTx.TxResult)
 	}
 
-	receipt := map[string]interface{}{
-		// Consensus fields: These fields are defined by the Yellow Paper
-		"status":            status,
-		"cumulativeGasUsed": hexutil.Uint64(cumulativeGasUsed),
-		"logsBloom":         bloom,
-		"logs":              logs,
-
-		// Implementation fields: These fields are added by geth when processing a transaction.
-		// They are stored in the chain database.
-		"transactionHash": tx.Hash,
-		"contractAddress": contractAddress,
-		"gasUsed":         hexutil.Uint64(resTx.TxResult.GasUsed),
-
-		// Inclusion information: These fields provide information about the inclusion of the
-		// transaction corresponding to this receipt.
-		"blockHash":        tx.BlockHash,
-		"blockNumber":      tx.BlockNumber,
-		"transactionIndex": tx.TransactionIndex,
-
-		// sender and receiver (contract or EOA) addresses
-		"from": tx.From,
-		"to":   tx.To,
+	receipt := &rpctypes.TransactionReceipt{
+		Status:            status,
+		CumulativeGasUsed: hexutil.Uint64(cumulativeGasUsed),
+		LogsBloom:         bloom,
+		Logs:              logs,
+		TransactionHash:   tx.Hash,
+		ContractAddress:   contractAddress,
+		GasUsed:           hexutil.Uint64(resTx.TxResult.GasUsed),
+		BlockHash:         *tx.BlockHash,
+		BlockNumber:       *tx.BlockNumber,
+		TransactionIndex:  *tx.TransactionIndex,
+		From:              tx.From,
+		To:                tx.To,
 	}
 
 	return receipt, nil
+}
+
+// GetTransactionByBlockHashAndIndex returns the transaction identified by block hash and index.
+func (svc *Service) GetTransactionByBlockHashAndIndex(hash common.Hash, idx hexutil.Uint64) (*rpctypes.Transaction, error) {
+	svc.mu.Lock()
+	defer svc.mu.Unlock()
+
+	svc.logger.Debug("eth_getTransactionByBlockHashAndIndex", "hash", hash, "idx", idx)
+	resBlock, err := svc.getTMClient().BlockByHash(hash.Bytes())
+	if err != nil {
+		svc.logger.Debug("eth_getTransactionByBlockHashAndIndex", "hash", hash, "idx", idx, "block not found")
+		return nil, err
+	}
+	return svc.getTransactionByBlockAndIndex(resBlock.Block, idx)
+}
+
+// GetTransactionByBlockNumberAndIndex returns the transaction identified by number and index.
+func (svc *Service) GetTransactionByBlockNumberAndIndex(blockNrOrHash rpc.BlockNumberOrHash, idx hexutil.Uint64) (*rpctypes.Transaction, error) {
+	svc.mu.Lock()
+	defer svc.mu.Unlock()
+
+	height, err := rpctypes.StateAndHeaderByNumberOrHash(svc.getTMClient(), blockNrOrHash)
+	if err != nil {
+		return nil, err
+	}
+	svc.logger.Debug("eth_getTransactionByBlockNumberAndIndex", "height", height, "idx", idx)
+
+	var (
+		blockNum int64
+	)
+
+	switch height {
+	case rpctypes.PendingBlockNumber:
+		blockNum = svc.getStateHeight(height)
+		svc.logger.Debug("eth_getTransactionByBlockNumberAndIndex", "height", blockNum, "idx", idx, "for pending txs")
+
+		result, err := svc.getTMClient().Block(&blockNum)
+		if err != nil {
+			return nil, err
+		}
+		if result.Block == nil {
+			svc.logger.Debug("eth_getTransactionByBlockNumberAndIndex", "height", blockNum, "idx", idx, "block not found with height")
+			return nil, nil
+		}
+
+		unconfirmed, err := svc.getTMClient().UnconfirmedTxs(1000)
+		if err != nil {
+			svc.logger.Debug("eth_getTransactionByBlockNumberAndIndex", "height", blockNum, "idx", idx, "failed to get unconfirmed txs", err)
+			return nil, err
+		}
+		// return if index out of bounds
+		if uint64(idx) >= uint64(len(unconfirmed.Txs)) {
+			return nil, nil
+		}
+		chainID := utils.HashToBigInt(result.Block.ChainID)
+
+		return rpctypes.LegacyRawBlockAndTxToEthTx(result.Block, &unconfirmed.Txs[idx], chainID, &idx)
+	case rpctypes.LatestBlockNumber:
+		blockNum = svc.getStateHeight(height)
+	default:
+		blockNum = height
+	}
+
+	result, err := svc.getTMClient().Block(&blockNum)
+	if err != nil {
+		return nil, err
+	}
+	return svc.getTransactionByBlockAndIndex(result.Block, idx)
+}
+
+func (svc *Service) getTransactionByBlockAndIndex(block *tmtypes.Block, idx hexutil.Uint64) (*rpctypes.Transaction, error) {
+	if block == nil {
+		svc.logger.Debug("getTransactionByBlockAndIndex", "block not found")
+		return nil, nil
+	}
+
+	// return if index out of bounds
+	if uint64(idx) >= uint64(len(block.Txs)) {
+		return nil, nil
+	}
+
+	chainID := utils.HashToBigInt(block.ChainID)
+	return rpctypes.LegacyRawBlockAndTxToEthTx(block, &block.Txs[idx], chainID, &idx)
 }
