6bd5b4447ede9db1537c4067a6c8c0ba4aa0e24e
DeV1doR <bogdankurinniy.dev1@gmail.com>
2021-07-08 14:34:43 +0300

    Add more methods for getting transactions

--- web3/types/types.go
+++ web3/types/types.go
@@ -6,6 +6,12 @@ import (
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 )
 
+const (
+	EarliestBlockNumber = 0
+	LatestBlockNumber   = -1
+	PendingBlockNumber  = -2
+)
+
 // CallArgs represents the arguments for a call.
 type CallArgs struct {
 	From     *common.Address `json:"from"`
@@ -56,4 +62,29 @@ type Transaction struct {
 	V                *hexutil.Big    `json:"v"`
 	R                *common.Hash    `json:"r"`
 	S                *common.Hash    `json:"s"`
+}
+
+// TransactionReceipt represents a mined transaction returned to RPC clients.
+type TransactionReceipt struct {
+	// Consensus fields: These fields are defined by the Yellow Paper
+	Status            hexutil.Uint64  `json:"status"`
+	CumulativeGasUsed hexutil.Uint64  `json:"cumulativeGasUsed"`
+	LogsBloom         ethtypes.Bloom  `json:"logsBloom"`
+	Logs              []*ethtypes.Log `json:"logs"`
+
+	// Implementation fields: These fields are added by geth when processing a transaction.
+	// They are stored in the chain database.
+	TransactionHash common.Hash     `json:"transactionHash"`
+	ContractAddress *common.Address `json:"contractAddress"`
+	GasUsed         hexutil.Uint64  `json:"gasUsed"`
+
+	// Inclusion information: These fields provide information about the inclusion of the
+	// transaction corresponding to this receipt.
+	BlockHash        common.Hash    `json:"blockHash"`
+	BlockNumber      hexutil.Big    `json:"blockNumber"`
+	TransactionIndex hexutil.Uint64 `json:"transactionIndex"`
+
+	// sender and receiver (contract or EOA) addresses
+	From common.Address  `json:"from"`
+	To   *common.Address `json:"to"`
 }
